cf1efe3addbd8bc8208ca687c14f3181588e9ff0
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2011-07-18 10:02:10 -0300

    Files generated by rails new.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sapos::Application.config.secret_token = '1e77226dd6f7166da819d93bcfdc2635aa253af90e06dc860062ca53babae21cd9f80bfb0d84327b0edd18e7c8db692d9d3212842d480debc77dfaed7bf18c2e'
