16dc74eb2cb7086364ecd244de8e59179fdd4508
Vanessa Braganholo Murta <vanessa@ic.uff.br>
2011-07-21 10:06:53 -0300

    Create index page pointing to enrollments.

--- config/routes.rb
+++ config/routes.rb
@@ -66,8 +66,8 @@ Sapos::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
+  #root :to => "welcome#index"
+  root :to => "enrollments", :action => "list"
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
