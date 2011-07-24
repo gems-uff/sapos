519ce80daba67b7c7ef7690dc22ddebf91d71178
Vanessa Braganholo Murta <vanessa@ic.uff.br>
2011-07-23 22:50:29 -0300

    Added validators for CPF and Enrollment_number.

--- app/helpers/enrollments_helper.rb
+++ app/helpers/enrollments_helper.rb
@@ -1,7 +1,2 @@
 module EnrollmentsHelper
- #def enrollment_status_form_column(record, options)
-    #check_box :record, :enrollment_status, options
-    #select_tag field_name, options_for_select('IBM' => '1', 'Lenovo' => '2')
-    #select_tag :record, :enrollment_status, options
- #end
 end
