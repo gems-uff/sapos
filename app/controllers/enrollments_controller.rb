9edfd16b33a58c8e9f124fbad73fc47fca083dad
Vanessa Braganholo Murta <vanessa@ic.uff.br>
2011-08-07 19:54:19 -0300

    Altered scholarship screen

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,12 +1,12 @@
 class EnrollmentsController < ApplicationController
   active_scaffold :enrollment do |config|
-    config.list.columns = [:student, :enrollment_number, :level, :enrollment_status, :admission_date, :dismissal, :obs]    
+    config.list.columns = [:student, :enrollment_number, :level, :enrollment_status, :admission_date, :dismissal]    
     config.list.sorting = {:enrollment_number => 'ASC'}
     config.create.label = :create_enrollment_label    
     config.columns[:level].form_ui = :select
     config.columns[:enrollment_status].form_ui = :select
-    #config.create.columns = [:enrollment_number, :admission_date, :level, :enrollment_status, :obs, :student, :dismissal]
-    #config.update.columns = [:enrollment_number, :admission_date, :level, :enrollment_status, :obs, :student, :dismissal]
+    config.create.columns = [:enrollment_number, :admission_date, :level, :enrollment_status, :obs, :student, :dismissal]
+    config.update.columns = [:enrollment_number, :admission_date, :level, :enrollment_status, :obs, :student, :dismissal]
     
     #the column group is being used to set up the position in which these fiels appear in the screen
     #config.create.columns.add_subgroup "Dados do Aluno" do |group|
--- app/controllers/scholarships_controller.rb
+++ app/controllers/scholarships_controller.rb
@@ -1,4 +1,12 @@
 class ScholarshipsController < ApplicationController
-  active_scaffold :scholarship do |conf|
+  active_scaffold :scholarship do |config|
+    config.list.sorting = {:scholarship_number => 'ASC'}
+    config.list.columns = [:scholarship_number, :level, :scholarship_type, :sponsor, :start_date, :end_date, :enrollments]
+    config.create.label = :create_scholarship_label
+    config.columns[:sponsor].form_ui = :select
+    config.columns[:scholarship_type].form_ui = :select
+    config.columns[:level].form_ui = :select
+    config.create.columns = [:scholarship_number, :level, :scholarship_type, :sponsor, :start_date, :end_date, :obs, :enrollments]
+    config.update.columns = [:scholarship_number, :level, :scholarship_type, :sponsor, :start_date, :end_date, :obs, :enrollments]
   end
 end 
