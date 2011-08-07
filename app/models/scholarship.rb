82d4b316d05d9e783cdc6bd42df13d7ca4d1e629
Vanessa Braganholo Murta <vanessa@ic.uff.br>
2011-08-07 20:20:34 -0300

    Seted up scholarship_duration screen and enrollment

--- app/models/scholarship.rb
+++ app/models/scholarship.rb
@@ -4,4 +4,8 @@ class Scholarship < ActiveRecord::Base
   belongs_to :scholarship_type
   has_many :scholarship_durations
   has_many :enrollments, :through => :scholarship_durations
+  
+  def to_label
+    "#{scholarship_number}"
+  end
 end
--- app/models/scholarship_duration.rb
+++ app/models/scholarship_duration.rb
@@ -1,4 +1,8 @@
 class ScholarshipDuration < ActiveRecord::Base
   belongs_to :scholarship
   belongs_to :enrollment
+  
+  def to_label
+    "#{start_date} - #{end_date}"
+  end
 end
