cc015065c4fa9a505bc774edc5dd85f26da41779
Vanessa Braganholo Murta <vanessa@ic.uff.br>
2011-08-07 22:06:29 -0300

    Added seeds to Sponsors, Dismissal_reasons, Enrollment_Type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
-  active_scaffold :user do |config|
+  active_scaffold :user do |config|    
     config.list.columns = [:name]
     config.create.columns = [:name, :password, :password_confirmation]
     config.update.columns = [:name, :password, :password_confirmation]
     config.update.label = :update_user_label
     config.create.label = :create_user_label
+    #config.columns[:password].form_ui = :password
+    #config.columns[:password_confirmation].form_ui = :password
   end
 end 
