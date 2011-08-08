58c1ad007619061071447c8d7655d72ae5aee551
Vanessa Braganholo Murta <vanessa@ic.uff.br>
2011-08-08 16:29:48 -0300

    Tryed to translate links without sucess.

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,7 +2,8 @@ class EnrollmentsController < ApplicationController
   active_scaffold :enrollment do |config|
     config.list.columns = [:student, :enrollment_number, :level, :enrollment_status, :admission_date, :dismissal]    
     config.list.sorting = {:enrollment_number => 'ASC'}
-    config.create.label = :create_enrollment_label    
+    config.create.label = :create_enrollment_label  
+    config.update.label = :update_enrollment_label
     config.columns[:level].form_ui = :record_select
     config.columns[:enrollment_status].form_ui = :record_select    
     #Student can not be configured as record select because it does not allow the user to create a new one, if needed
