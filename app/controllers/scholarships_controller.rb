fe169be69e276dab352fe0fae906b6d2d5ed42b1
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2011-08-13 12:34:53 -0300

    Showing sponsor before scholarship_type.

--- app/controllers/scholarships_controller.rb
+++ app/controllers/scholarships_controller.rb
@@ -1,15 +1,15 @@
 class ScholarshipsController < ApplicationController
   active_scaffold :scholarship do |config|
     config.list.sorting = {:scholarship_number => 'ASC'}
-    config.list.columns = [:scholarship_number, :level, :scholarship_type, :sponsor, :start_date, :end_date]
+    config.list.columns = [:scholarship_number, :level, :sponsor, :scholarship_type, :start_date, :end_date]
     config.create.label = :create_scholarship_label
     config.columns[:sponsor].form_ui = :select
     config.columns[:scholarship_type].form_ui = :select
     config.columns[:level].form_ui = :select
     config.columns[:professor].form_ui = :record_select
-    config.create.columns = [:scholarship_number, :level, :scholarship_type, :sponsor, :professor, :start_date, :end_date, :obs, :enrollments]
-    config.update.columns = [:scholarship_number, :level, :scholarship_type, :sponsor, :professor, :start_date, :end_date, :obs, :enrollments]
-    config.show.columns = [:scholarship_number, :level, :scholarship_type, :sponsor, :professor, :start_date, :end_date, :obs, :enrollments]
+    config.create.columns = [:scholarship_number, :level, :sponsor, :scholarship_type, :professor, :start_date, :end_date, :obs, :enrollments]
+    config.update.columns = [:scholarship_number, :level, :sponsor, :scholarship_type, :professor, :start_date, :end_date, :obs, :enrollments]
+    config.show.columns = [:scholarship_number, :level, :sponsor, :scholarship_type, :professor, :start_date, :end_date, :obs, :enrollments]
   end
   record_select :per_page => 10, :search_on => [:scholarship_number], :order_by => 'scholarship_number', :full_text_search => true
 end 
