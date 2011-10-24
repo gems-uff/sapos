8e2de97d00d77c901eca4de9679f84d2a3af56bc
Rodrigo <rdias.ferreira@gmail.com>
2011-10-24 10:50:17 -0200

    Adição do seeds.rb original.

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+# 
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create(:name => 'admin', :password => 'admin')
+
+Sponsor.create(:name => "CAPES")
+Sponsor.create(:name => "CNPq")
+Sponsor.create(:name => "FAPERJ")
+Sponsor.create(:name => "PETROBRAS")
+
+Level.create(:name => "Graduação")
+Level.create(:name => "Especialização")
+Level.create(:name => "Mestrado")
+Level.create(:name => "Doutorado")
+
+EnrollmentStatus.create(:name => "Especial")
+EnrollmentStatus.create(:name => "Regular")
+
+DismissalReason.create(:name => "Defesa", :description => "Aluno defendeu")
+DismissalReason.create(:name => "Rendimento", :description => "Aluno não cumpriu critérios de rendimento")
+DismissalReason.create(:name => "Desistência", :description => "Aluno desistiu")
+DismissalReason.create(:name => "Prazo", :description => "Prazo para defesa esgotado")
+DismissalReason.create(:name => "Especial -> Regular", :description => "Aluno foi admitido como aluno regular")
