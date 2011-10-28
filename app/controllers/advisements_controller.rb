712ff61f3c6dc3b777b0c6cf18937ef7cf8e623c
Tiago Amaro <tiagocis@gmail.com>
2011-10-27 22:54:15 -0200

    - Ordem alfabética de nomes dos professores (Bug #11) consertado.   Adição de Include no SQL de ordenação feita pelo Rails

--- app/controllers/advisements_controller.rb
+++ app/controllers/advisements_controller.rb
@@ -1,6 +1,8 @@
 class AdvisementsController < ApplicationController
   active_scaffold :advisement do |config|    
     config.list.columns = [:professor, :enrollment, :main_advisor]
+    config.columns[:professor].includes = [:professor]
+    config.columns[:professor].sort_by :sql => 'professors.name'
     config.list.sorting = {:enrollment => 'ASC'}
     config.create.label = :create_advisement_label        
     config.columns[:professor].form_ui = :record_select
