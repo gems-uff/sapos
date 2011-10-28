ed6fb527baf7eb1d3f7655f67f3578c86188c0cd
Tiago Amaro <tiagocis@gmail.com>
2011-10-28 12:04:00 -0200

    - Instituições estão agora mostrando toda a lista de cursos quando clicamos em Visualizar - A lista de orientandos de professores está correta, está sendo mostrada uma lista completa da matrícula com o nome do aluno de cada professor (Sessão Professores -> Professores)

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -4,7 +4,7 @@ class InstitutionsController < ApplicationController
     config.list.columns = [:name, :code]
     config.create.label = :create_institution_label
     config.columns = [:name, :code, :courses]    
-
+    config.columns[:courses].associated_limit = nil
   end
-  record_select :per_page => 10, :search_on => [:name], :order_by => 'name', :full_text_search => true
+  record_select :per_page => 10, :search_on => [:name], :order_by => 'name', :full_text_search => true  
 end 
--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -5,7 +5,24 @@ class ProfessorsController < ApplicationController
     config.create.label = :create_professor_label    
 #    config.create.columns = [:name, :cpf, :birthdate]
 #    config.update.columns = [:name, :cpf, :birthdate]
-#    config.show.columns = [:name, :cpf, :birthdate, :scholarships, :advisements]    
+    config.columns[:enrollments].associated_limit = nil
+    config.show.columns = [:name,
+                           :cpf,
+                           :birthdate,
+                           :address,
+                           :birthdate,
+                           :civil_status,
+                           :identity_expedition_date,
+                           :identity_issuing_body,
+                           :identity_number,
+                           :neighbourhood,
+                           :sex,
+                           :siape,
+                           :telephone1,
+                           :telephone2,
+                           :zip_code,                           
+                           :scholarships, 
+                           :enrollments]    
   end
   record_select :per_page => 10, :search_on => [:name], :order_by => 'name', :full_text_search => true
 end 
