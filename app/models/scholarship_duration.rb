cfcff67085ba496ffb32d02f64bd36f6df810c30
Tiago Amaro <tiagocis@gmail.com>
2011-11-06 01:46:29 -0200

    - Validação de data inicial e final de Alocação de Bolsas

--- app/models/scholarship_duration.rb
+++ app/models/scholarship_duration.rb
@@ -5,4 +5,12 @@ class ScholarshipDuration < ActiveRecord::Base
   def to_label
     "#{start_date} - #{end_date}"
   end
+  
+  validate :start_date_greater_than_end_date
+  
+  def start_date_greater_than_end_date
+    if start_date.to_date > end_date.to_date
+      errors.add(:start_date,I18n.t("activerecord.errors.scholarship_duration.start_date_greater_than_end_date"))
+    end
+  end
 end
