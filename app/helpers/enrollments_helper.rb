b411e613d3fb7b5bc39cf9c9b693b750bafb88f3
Tiago Amaro <tiagocis@gmail.com>
2011-11-12 14:54:01 -0200

    - Adicionada a verificação de um orientador principal por matrícula (Bug #19) - Adicionada date_select em Bolsas (enrollments_helpers/enrollments_helper.rb)

--- app/helpers/enrollments_helper.rb
+++ app/helpers/enrollments_helper.rb
@@ -1,2 +1,30 @@
 module EnrollmentsHelper
+  @@config = YAML::load_file("#{Rails.root}/config/properties.yml")    
+  @@range = @@config["scholarship_year_range"]    
+      
+  def cancel_date_form_column(record,options)                
+    date_select :record, :cancel_date, {
+         :discard_day => true,
+         :start_year => Time.now.year - @@range,
+         :end_year => Time.now.year + @@range,
+         :include_blank => true,
+         :default => nil  
+    }    
+  end
+  
+  def start_date_form_column(record,options)                
+    date_select :record, :start_date, {
+         :discard_day => true,
+         :start_year => Time.now.year - @@range,
+         :end_year => Time.now.year + @@range
+    }    
+  end
+  
+  def end_date_form_column(record,options)    
+    date_select :record, :end_date, {
+         :discard_day => true,
+         :start_year => Time.now.year - @@range,
+         :end_year => Time.now.year + @@range
+    }        
+  end    
 end
--- app/models/advisement.rb
+++ app/models/advisement.rb
@@ -5,4 +5,11 @@ class Advisement < ActiveRecord::Base
   def to_label
     "#{enrollment.enrollment_number} - #{professor.name}"    
   end
+  
+  validate :one_main_advisor_per_enrollment
+  
+  def one_main_advisor_per_enrollment    
+    old_main_advisement = Advisement.find_by_sql(["select * from advisements where enrollment_id = ? and main_advisor = true",enrollment.id]) unless enrollment.nil?
+    errors.add(:enrollment,I18n.t("activerecord.errors.advisement.already_with_main_advisor")) if (not old_main_advisement.empty? and main_advisor)
+  end
 end
