29e9f474592bab5233f26f10beacd631c8b18804
Rodrigo <rdias.ferreira@gmail.com>
2011-11-15 15:24:29 -0200

    Some adjustments in phases_controller.

--- app/controllers/phases_controller.rb
+++ app/controllers/phases_controller.rb
@@ -4,8 +4,9 @@ class PhasesController < ApplicationController
     config.list.columns = [:name, :description]
     config.create.label = :create_phase_label
     config.columns[:enrollments].form_ui = :record_select
-    config.create.columns = [:name, :description, :accomplishments]
-    config.update.columns = [:name, :description, :accomplishments]
+#    config.columns[:levels].form_ui = :select
+    config.create.columns = [:name, :description, :phase_durations, :accomplishments]
+    config.update.columns = [:name, :description, :phase_durations, :accomplishments]
     config.show.columns = [:name, :description, :enrollments]
   end
 #  record_select :per_page => 10, :search_on => [:name], :order_by => 'name', :full_text_search => true
--- app/models/phase.rb
+++ app/models/phase.rb
@@ -1,4 +1,6 @@
 class Phase < ActiveRecord::Base
   has_many :accomplishments, :dependent => :destroy
   has_many :enrollments, :through => :accomplishments
+  has_many :phase_durations, :dependent => :destroy
+  has_many :levels, :through => :phase_durations
 end
