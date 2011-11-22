ebb3e2b5b33aab8697e78c9b517890733dc07a1e
Tiago Amaro <tiagocis@gmail.com>
2011-11-22 21:03:26 -0200

    -Refatoração da validação de um orientador principal por matrícula -Adicionada a validação que informa se o mesmo professor já não é orientador da matrícula

--- app/models/advisement.rb
+++ app/models/advisement.rb
@@ -6,12 +6,6 @@ class Advisement < ActiveRecord::Base
     "#{enrollment.enrollment_number} - #{professor.name}"    
   end
   
-    validates :professor_id, :uniqueness => {:scope => :enrollment_id} #A professor can't be advisor more than once of an enrollment
-  
-#  validate :one_main_advisor_per_enrollment
-  
-#  def one_main_advisor_per_enrollment    
-#    old_main_advisement = Advisement.find_by_sql(["select * from advisements where enrollment_id = ? and main_advisor = 1",enrollment.id]) unless enrollment.nil?
-#    errors.add(:enrollment,I18n.t("activerecord.errors.advisement.already_with_main_advisor")) if (not old_main_advisement.empty? and main_advisor)
-#  end
+  validates :professor_id, :uniqueness => {:scope => :enrollment_id} #A professor can't be advisor more than once of an enrollment
+  validates :main_advisor, :uniqueness => {:scope => :enrollment_id}
 end
