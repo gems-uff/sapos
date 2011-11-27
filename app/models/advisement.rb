7cf401fc8a3288baab898f8d7742f6cb623b307b
Tiago Amaro <tiagocis@gmail.com>
2011-11-26 23:46:51 -0200

    - Modificação em validação de único orientador (Apenas verifica se já existe um orientador principal se o usuário tentar incluir outro orientador principal)

--- app/models/advisement.rb
+++ app/models/advisement.rb
@@ -7,5 +7,5 @@ class Advisement < ActiveRecord::Base
   end
   
   validates :professor_id, :uniqueness => {:scope => :enrollment_id} #A professor can't be advisor more than once of an enrollment
-  validates :main_advisor, :uniqueness => {:scope => :enrollment_id}
+  validates :main_advisor, :uniqueness => {:scope => :enrollment_id}, :if => :main_advisor #only check if main_advisor is unique if the user tries to insert another main advisor
 end
