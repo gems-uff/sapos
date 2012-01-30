3f773ec6b46e16f4377d3075219f8a34ac0cb5af
Tiago Amaro <tiagocis@gmail.com>
2012-01-30 19:36:37 -0200

    -Na sessão Configurações -> Tipos de Prorrogação adicionar a coluna que mostra a que etapa esta prorrogação está associada (Bug #24)

--- app/controllers/deferral_types_controller.rb
+++ app/controllers/deferral_types_controller.rb
@@ -1,8 +1,9 @@
 class DeferralTypesController < ApplicationController
   active_scaffold :deferral_type do |config|
     config.list.sorting = {:name => 'ASC'}
-    config.list.columns = [:name, :description, :duration]
+    config.list.columns = [:name, :description, :duration, :phase]
     config.create.label = :create_deferral_type_label
+    config.columns[:phase].clear_link
     config.columns[:phase].form_ui = :select
     config.create.columns = [:name, :description, :duration, :phase]
     config.update.columns = [:name, :description, :duration, :phase]
