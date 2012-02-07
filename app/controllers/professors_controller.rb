62c2d6967086fac26511fe517735d728b0aeafb7
Tiago Amaro <tiagocis@gmail.com>
2012-02-07 00:14:24 -0200

    - Pontos de orientação

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,6 +1,6 @@
 class ProfessorsController < ApplicationController
   active_scaffold :professor do |config|
-    config.list.columns = [:name, :cpf, :birthdate]    
+    config.list.columns = [:name, :cpf, :birthdate, :advisement_points]
     config.list.sorting = {:name => 'ASC'}
     config.create.label = :create_professor_label    
 #    config.create.columns = [:name, :cpf, :birthdate]
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -5,4 +5,23 @@ class Professor < ActiveRecord::Base
 
   validates :cpf, :presence => true, :uniqueness => true
   validates :name, :presence => true
+  
+#  It was considered that active advisements were enrollments without dismissals reasons
+  def advisement_points
+    search_sql = ["SELECT *
+                  FROM  advisements AS a,
+                      enrollments AS e
+                  LEFT OUTER JOIN dismissals AS d
+                  ON    d.enrollment_id = e.id
+                  WHERE a.enrollment_id = e.id
+                  AND   d.id IS NULL
+                  AND   a.professor_id = ?",self.id]
+    points = 0
+    advisements = Advisement.find_by_sql(search_sql)
+    advisements.each do |adv| 
+      adv.main_advisor ? points += 1 : points += 0.5
+    end
+    
+    "#{points}"
+  end
 end
