9f284eabfd78e99d9c06a5315d4eeb2886b97eae
Tiago Amaro <tiagocis@gmail.com>
2012-02-09 23:44:22 -0200

    - Obrigatoriedade de uma orientação possuir um orientador principal

--- app/models/advisement.rb
+++ app/models/advisement.rb
@@ -8,4 +8,10 @@ class Advisement < ActiveRecord::Base
   
   validates :professor_id, :uniqueness => {:scope => :enrollment_id} #A professor can't be advisor more than once of an enrollment
   validates :main_advisor, :uniqueness => {:scope => :enrollment_id}, :if => :main_advisor #only check if main_advisor is unique if the user tries to insert another main advisor
+  validates :main_advisor, :presence => true, :if => :enrollment_has_no_advisors
+  
+  def enrollment_has_no_advisors
+    return true if enrollment.nil?
+    Advisement.find_all_by_enrollment_id(enrollment.id).empty?
+  end
 end
