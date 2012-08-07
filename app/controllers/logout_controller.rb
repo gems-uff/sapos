3b69ad111f6b5402b73a6dfe75f3e15aba27bf34
tiagoamaro <tiagocis@gmail.com>
2012-08-07 16:02:52 -0300

    - Deleção de controlador não utilizado

--- app/controllers/logout_controller.rb
+++
@@ -1,5 +0,0 @@
-class LogoutController < ApplicationController
-  def logout
-    sign_out_and_redirect(current_user) if current_user && session[:sign_in_token] != current_user.current_sign_in_token
-  end
-end
