55e1d40b755749e25c279ac2a4ab69725dfc5e14
tiago <tiagocis@gmail.com>
2012-08-21 20:08:10 -0300

    - Reordenação de navegação de menú principal e submenus

--- config/navigation.rb
+++ config/navigation.rb
@@ -44,39 +44,39 @@ SimpleNavigation::Configuration.run do |navigation|
     #                            when the item should be highlighted, you can set a regexp which is matched
     #                            against the current URI.
     #
+    primary.item :stud, 'Alunos', enrollments_path do |stud|
+      stud.item :student, 'Alunos', students_path
+      stud.item :scholarship_duration, 'Alocação de Bolsas', scholarship_durations_path
+      stud.item :scholarship, 'Bolsas', scholarships_path
+      stud.item :dismissal, 'Desligamentos', dismissals_path
+      stud.item :enrollment, 'Matrículas', enrollments_path
+      stud.item :deferral, 'Prorrogações', deferrals_path
+      stud.item :accomplishment, 'Realização de Etapa', accomplishments_path
+    end
+
     primary.item :prof, 'Professores', professors_path do |prof|
       prof.item :professor, 'Professores', professors_path     
       prof.item :advisement, 'Orientações', advisements_path
     end
 
-    primary.item :stud, 'Alunos', enrollments_path do |stud|
-      stud.item :student, 'Alunos', students_path
-      stud.item :enrollment, 'Matrículas', enrollments_path
-      stud.item :accomplishment, 'Realização de Etapa', accomplishments_path
-      stud.item :scholarship, 'Bolsas', scholarships_path      
-      stud.item :scholarship_duration, 'Alocação de Bolsas', scholarship_durations_path
-      stud.item :deferral, 'Prorrogações', deferrals_path
-      stud.item :dismissal, 'Desligamentos', dismissals_path
-    end
-        
     primary.item :inst, 'Instituições e Cursos', institutions_path do |inst|
-      inst.item :institution, 'Instituições', institutions_path
       inst.item :course, 'Cursos', courses_path
+      inst.item :institution, 'Instituições', institutions_path
     end
     
     primary.item :configuration, 'Configurações', sponsors_path do |configuration|
-      configuration.item :phase, 'Etapas', phases_path
       configuration.item :sponsor, 'Agências de Fomento', sponsors_path
-      configuration.item :level, 'Níveis', levels_path   
+      configuration.item :phase, 'Etapas', phases_path
+      configuration.item :level, 'Níveis', levels_path
       configuration.item :dismissal_reason, 'Razões de Desligamento', dismissal_reasons_path
       configuration.item :scholarship_type, 'Tipos de Bolsa', scholarship_types_path
-      configuration.item :deferral_type, 'Tipos de Prorrogação', deferral_types_path      
+      configuration.item :deferral_type, 'Tipos de Prorrogação', deferral_types_path
       configuration.item :enrollment_status, 'Tipos de Matrícula', enrollment_statuses_path
       configuration.item :user, 'Usuários', users_path
-      configuration.item :city, 'Cidades', cities_path 
-      configuration.item :state, 'Estados', states_path 
       configuration.item :country, 'Países', countries_path
-    end    
+      configuration.item :state, 'Estados', states_path
+      configuration.item :city, 'Cidades', cities_path
+    end
 
     # Add an item which has a sub navigation (same params, but with block)
     #primary.item :key_2, 'name', url, options do |sub_nav|
