f4e11bd27847a734687f46bf13f63a5d2aeee6c7
Everton Moreth <everton.moreth@gmail.com>
2013-01-18 16:00:11 -0200

    Update config/initializers/session_store.rb
    Separando cookies de producao e homologacao

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Sapos::Application.config.session_store :cookie_store, :key => '_sapos_session'
+if Rails.env.production?
+  Sapos::Application.config.session_store :cookie_store, :key => '_sapos_session'
+else
+  Sapos::Application.config.session_store :cookie_store, :key => '_sapos_session_h'
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
