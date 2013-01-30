05a45e66a1cfa7c8f197610797e3156a5750f94e
Bruno Schettino <bschettino@id.uff.br>
2013-01-30 18:52:50 -0200

    Adding delayed phases search

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -10,16 +10,58 @@ class Enrollment < ActiveRecord::Base
   has_many :accomplishments, :dependent => :destroy
   has_many :phases, :through => :accomplishments
   has_many :deferrals, :dependent => :destroy
-     
+
   def to_label
     "#{enrollment_number} - #{student.name}"
   end
-  
+
   validates_associated :advisements
   validates_associated :scholarship_durations
-             
+
   validates :enrollment_number, :presence => true, :uniqueness => true
   validates :level, :presence => true
-  validates :enrollment_status, :presence => true  
+  validates :enrollment_status, :presence => true
   validates :student, :presence => true
+
+  def self.with_delayed_phases_on(date, phase = nil)
+    date = Date.today if date.nil?
+    phase = Phase.all if phase.nil?
+    enrollments = Enrollment.where("enrollments.id not in (select enrollment_id from dismissals where DATE(dismissals.date) <= DATE(?))", date)
+
+    delayed_enrollments = []
+
+    enrollments.each do |enrollment|
+
+      phases_duration = PhaseDuration.where(:level_id => enrollment.level_id, :phase_id => phase)
+
+      begin_ys = YearSemester.on_date(enrollment.admission_date)
+
+      phases_duration.each do |phase_duration|
+
+        phase_duration_deadline_ys = begin_ys + phase_duration.deadline_semesters
+        phases_duration_deadline_months = phase_duration.deadline_months
+        phases_duration_deadline_days = phase_duration.deadline_days
+
+        deferral_types = DeferralType.joins(:deferrals).where("deferrals.enrollment_id = #{enrollments.id}", :phase_id => phase_duration.phase_id)
+
+        final_ys = phase_duration_deadline_ys
+        final_months = phases_duration_deadline_months
+        final_days = phases_duration_deadline_days
+
+        deferral_types.each do |deferral_type|
+          final_ys.increase_semesters(deferral_type.duration_semesters)
+          final_months += deferral_type.duration_months
+          final_days += deferral_type.duration_days
+        end
+
+        deadline_date = final_ys.semester_end + final_months.months + final_days.days
+
+        if deadline_date <= date
+          delayed_enrollments << enrollment.id
+          break
+        end
+      end
+    end
+    delayed_enrollments
+  end
 end
