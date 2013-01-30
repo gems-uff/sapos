02965d7c92e1e226bd02eb028840bf7f76704e28
Bruno Schettino <bschettino@id.uff.br>
2013-01-30 18:50:02 -0200

    Adding days and months to deferral_types views

--- app/controllers/deferral_types_controller.rb
+++ app/controllers/deferral_types_controller.rb
@@ -1,12 +1,12 @@
 class DeferralTypesController < ApplicationController
   active_scaffold :deferral_type do |config|
     config.list.sorting = {:name => 'ASC'}
-    config.list.columns = [:name, :description, :duration, :phase]
+    config.list.columns = [:name, :description, :duration_semesters, :duration_months, :duration_days, :phase]
     config.create.label = :create_deferral_type_label
     config.columns[:phase].clear_link
     config.columns[:phase].form_ui = :select
-    config.create.columns = [:name, :description, :duration, :phase]
-    config.update.columns = [:name, :description, :duration, :phase]
-    config.show.columns = [:name, :description, :duration, :phase]
+    config.create.columns = [:name, :description, :duration_semesters, :duration_months, :duration_days, :phase]
+    config.update.columns = [:name, :description, :duration_semesters, :duration_months, :duration_days, :phase]
+    config.show.columns = [:name, :description, :duration_semesters, :duration_months, :duration_days, :phase]
   end
 end
