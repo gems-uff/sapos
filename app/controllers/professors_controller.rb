fc5d4009837a40a99c0f0c551e834b02157ed154
Bruno Schettino <bschettino@id.uff.br>
2013-02-13 15:35:33 -0200

    Advisement Authorizations form

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -34,7 +34,8 @@ class ProfessorsController < ApplicationController
                            :telephone2,
                            :zip_code,                           
                            :scholarships, 
-                           :enrollments]    
+                           :enrollments,
+                           :advisement_authorizations]
   end
   record_select :per_page => 10, :search_on => [:name], :order_by => 'name', :full_text_search => true
 end 
