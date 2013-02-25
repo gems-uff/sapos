43dee99bae49959cac46928f9264d9458014899b
Bruno Schettino <bschettino@id.uff.br>
2013-02-25 19:50:24 -0300

    Trying to fix advisements points

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -9,7 +9,7 @@ class Professor < ActiveRecord::Base
 
 #  It was considered that active advisements were enrollments without dismissals reasons
   def advisement_points
-    return "#{0}" if self.advisement_authorizations.empty?
+    return "#{0.0}" if self.advisement_authorizations.empty?
 
     enrollments = Enrollment.joins(["LEFT OUTER JOIN dismissals ON enrollments.id = dismissals.enrollment_id", :advisements]).
         where("advisements.professor_id = :professor_id AND dismissals.id IS NULL", :professor_id => self.id).scoped
@@ -18,7 +18,7 @@ class Professor < ActiveRecord::Base
 
     enrollments_with_multiple_advisors = enrollments.where("1 < (SELECT COUNT(*) FROM advisements WHERE advisements.enrollment_id = enrollments.id AND advisements.professor_id in (SELECT advisement_authorizations.professor_id from advisement_authorizations))")
 
-    points = 0
+    points = 0.0
     points += 0.5*enrollments_with_multiple_advisors.count + enrollments_with_single_advisor.count
 
     "#{points.to_f}"
@@ -29,9 +29,9 @@ class Professor < ActiveRecord::Base
   end
 
   def advisement_point(enrollment)
-    return 0 if self.advisement_authorizations.empty? || enrollment.advisements.where(:professor_id => self.id).empty?
+    return 0.0 if self.advisement_authorizations.empty? || enrollment.advisements.where(:professor_id => self.id).empty? || enrollment.dismissal
     authorized_advisors = enrollment.advisements.joins([:professor => :advisement_authorizations]).count
-    (authorized_advisors == 1) ? 1.0 : 0.5
+    (authorized_advisors.to_i == 1) ? 1.0 : 0.5
   end
 
   def advisements_with_points
@@ -39,7 +39,7 @@ class Professor < ActiveRecord::Base
 
     body = ""
     count = 0
-    total_point = 0
+    total_point = 0.0
     self.advisements.joins([:enrollment, "LEFT OUTER JOIN dismissals ON enrollments.id = dismissals.enrollment_id"]).where("dismissals.id IS NULL").each do |advisement|
       count +=1;
       tr_class = count.even? ? "even-record" : ""
