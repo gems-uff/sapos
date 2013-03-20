9f6c789ec0a4add3775c7182ef4ec78be4ac171f
Everton Moreth <everton.moreth@gmail.com>
2013-03-20 01:22:50 -0300

    Cada tipo de permissao tem o root que tem acesso

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,19 +13,24 @@ class ApplicationController < ActionController::Base
     config.delete.link.label = :delete_link
     config.show.link.label = :show_link
     config.update.link.label = :update_link
-    config.search.link.label = :search_link    
-    config.delete.link.confirm = :delete_message       
+    config.search.link.label = :search_link
+    config.delete.link.confirm = :delete_message
     #Faz com que a busca seja enviada ao servidor enquanto o usuário digita
-    config.search.live = true              
+    config.search.live = true
     #config.search.link.cancel = false        
   end
-  
+
   # Defines which controller and action should be shown when the base
   # URL is acessed (root route in routes.rb).
   def root
-    redirect_to :controller => 'enrollments', :action => 'index'
+    [Enrollment, Professor, ScholarshipDuration, Phase, Course, City, User].each do |model|
+      if can? :read, model
+        redirect_to(:controller => model.name.underscore.pluralize.downcase, :action => 'index') and return
+      end
+    end
+
   end
-  
+
   private
 
   #def authenticate
@@ -39,7 +44,7 @@ class ApplicationController < ActionController::Base
     if params[:record]
       external_key = "";
       invalid_year = false;
-      params[:record].each {|key,value|
+      params[:record].each { |key, value|
         if key.include?("1i")
           invalid_year = value.to_i < 1000
           external_key = key.split("(")[0]
@@ -47,7 +52,7 @@ class ApplicationController < ActionController::Base
 
         if invalid_year
           invalid_year = false
-          params[:record].delete_if{|key,value|
+          params[:record].delete_if { |key, value|
             key.include?(external_key)
           }
 
