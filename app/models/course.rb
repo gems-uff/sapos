26e87907e0fafaeb11c7c6af4ef7fa8a8073ca0e
Bruno Schettino <bschettino@id.uff.br>
2013-04-11 00:53:46 -0300

    Add course uniqueness validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ActiveRecord::Base
   belongs_to :course_type
 
   validates :course_type, :presence => true
-  validates :name, :presence => true
-  validates :code, :presence => true
+  validates :name, :presence => true, :uniqueness => true
+  validates :code, :presence => true, :uniqueness => true
   validates :credits, :presence => true
 end
