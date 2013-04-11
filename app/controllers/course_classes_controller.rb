c46f00c6fd34e6b2883b1adc1733db0d3f4bb7e0
Bruno Schettino <bschettino@id.uff.br>
2013-04-11 00:50:34 -0300

    Add course classes controller config

--- app/controllers/course_classes_controller.rb
+++ app/controllers/course_classes_controller.rb
@@ -1,4 +1,24 @@
 class CourseClassesController < ApplicationController
-  active_scaffold :course_class do |conf|
+  active_scaffold :course_class do |config|
+    config.list.sorting = {:name => 'ASC'}
+    config.list.columns = [:name, :course, :professor, :year, :semester]
+    config.create.label = :create_course_class_label
+    config.update.label = :update_course_class_label
+
+    config.columns[:course].clear_link
+    config.columns[:professor].clear_link
+    config.columns[:course].form_ui = :record_select
+    config.columns[:professor].form_ui = :record_select
+    config.columns[:year].form_ui = :select
+    config.columns[:semester].form_ui = :select
+    config.columns[:semester].options = {:options => ['1','2']}
+    config.columns[:year].options = {:options => ((Date.today.year-5)..Date.today.year).map {|y| y}.reverse}
+
+    config.create.columns =
+        [:name, :course, :professor, :year, :semester]
+
+    config.update.columns =
+        [:name, :course, :professor, :class_enrollments, :allocations, :year, :semester]
   end
+  record_select :per_page => 10, :search_on => [:name], :order_by => 'name', :full_text_search => true
 end 
