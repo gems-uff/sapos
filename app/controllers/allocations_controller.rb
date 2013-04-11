d381e41d0c897ba0760121405fff812480e3cfc8
Bruno Schettino <bschettino@id.uff.br>
2013-04-11 00:48:58 -0300

    Add allocations controller config

--- app/controllers/allocations_controller.rb
+++ app/controllers/allocations_controller.rb
@@ -1,4 +1,20 @@
 class AllocationsController < ApplicationController
-  active_scaffold :allocation do |conf|
+  active_scaffold :allocation do |config|
+    config.list.sorting = {:course_class => 'ASC'}
+    config.list.columns = [:course_class, :day, :room, :start_time, :end_time]
+    config.create.label = :create_allocation_label
+    config.update.label = :update_allocation_label
+
+    config.columns[:course_class].clear_link
+    config.columns[:course_class].form_ui = :record_select
+    config.columns[:start_time].options = {:format => :short}
+    config.columns[:end_time].options = {:format => :short}
+    config.columns[:day].form_ui = :select
+    config.columns[:day].options = {:options => I18n.translate("date.day_names")}
+
+    config.columns =
+        [:course_class, :day, :room, :start_time, :end_time]
+
   end
-end 
+  record_select :per_page => 10, :search_on => [:name], :order_by => 'name', :full_text_search => true
+end
