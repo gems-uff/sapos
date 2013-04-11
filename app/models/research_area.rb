b985b5b6ab95e52c86f9e343031211eb01887bf0
Bruno Schettino <bschettino@id.uff.br>
2013-04-11 00:56:22 -0300

    Add research area uniqueness validation

--- app/models/research_area.rb
+++ app/models/research_area.rb
@@ -2,8 +2,8 @@ class ResearchArea < ActiveRecord::Base
   has_many :courses
   has_many :professors, :through => :professor_research_areas
 
-  validates :name, :presence => true
-  validates :code, :presence => true
+  validates :name, :presence => true, :uniqueness => true
+  validates :code, :presence => true, :uniqueness => true
 
   def to_label
     "#{code} - #{name}"
