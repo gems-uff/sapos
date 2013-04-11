1c3076121d5c5957bec7780a3e8c60e547b76361
Bruno Schettino <bschettino@id.uff.br>
2013-04-11 00:53:09 -0300

    Add class enrollment validations

--- app/models/class_enrollment.rb
+++ app/models/class_enrollment.rb
@@ -1,4 +1,16 @@
 class ClassEnrollment < ActiveRecord::Base
   belongs_to :course_class
   belongs_to :enrollment
+
+  SITUATIONS = [I18n.translate("activerecord.attributes.class_enrollment.situations.registered"), I18n.translate("activerecord.attributes.class_enrollment.situations.aproved"), I18n.translate("activerecord.attributes.class_enrollment.situations.disapproved")]
+
+  validates :enrollment, :presence => true
+  validates :course_class, :presence => true
+  validates :situation, :presence => true, :inclusion => { :in => SITUATIONS }
+  validates :grade, :numericality => {:greater_than_or_equal_to => 0, :less_than_or_equal_to => 100}, :if => :grade_filled?
+
+  def grade_filled?
+    !grade.nil?
+  end
+
 end
