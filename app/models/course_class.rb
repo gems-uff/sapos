a162781ddbf634274462e9e06448c159e4002b45
Bruno Schettino <bschettino@id.uff.br>
2013-04-11 00:54:34 -0300

    Add course class has_many associations, validations and to_label method

--- app/models/course_class.rb
+++ app/models/course_class.rb
@@ -1,4 +1,16 @@
 class CourseClass < ActiveRecord::Base
   belongs_to :course
   belongs_to :professor
+
+  has_many :class_enrollments
+  has_many :allocations
+
+  validates :course, :presence => true
+  validates :professor, :presence => true
+  validates :year, :presence => true
+  validates :semester, :presence => true
+
+  def to_label
+    "#{name || course.name} - #{year}/#{semester}"
+  end
 end
