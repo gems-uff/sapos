a06d4db29aa49d94b674bda784f79fc162c1f8e7
Bruno Schettino <bschettino@id.uff.br>
2013-04-12 11:13:18 -0300

    advisement authorization tests

---
+++ spec/models/advisement_authorization_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe AdvisementAuthorization do
+  let(:advisement_authorization) { AdvisementAuthorization.new }
+  subject { advisement_authorization }
+  describe "Validations" do
+    describe "professor" do
+      context "should be valid when" do
+        it "professor is not null" do
+          advisement_authorization.professor = Professor.new
+          advisement_authorization.should have(0).errors_on :professor
+        end
+      end
+      context "should have error blank when" do
+        it "professor is null" do
+          advisement_authorization.professor = nil
+          advisement_authorization.should have_error(:blank).on :professor
+        end
+      end
+    end
+    describe "level" do
+      context "should be valid when" do
+        it "level is not null" do
+          advisement_authorization.level = Level.new
+          advisement_authorization.should have(0).errors_on :level
+        end
+      end
+      context "should have error blank when" do
+        it "level is null" do
+          advisement_authorization.level = nil
+          advisement_authorization.should have_error(:blank).on :level
+        end
+      end
+    end
+  end
+  describe "Methods" do
+    describe "to_label" do
+      it "should return the expected string" do
+        level_name = "AuthorizedLevel"
+        advisement_authorization.level = Level.new(:name => level_name)
+        advisement_authorization.to_label.should eql(level_name)
+      end
+    end
+  end
+end
