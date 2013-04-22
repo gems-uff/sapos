5252660b989dbe54ffde7d2d262e67e10e432c28
Bruno Schettino <bschettino@id.uff.br>
2013-04-22 19:01:05 -0300

    Enrollment Status tests

---
+++ spec/models/enrollment_status_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+
+describe EnrollmentStatus do
+  let(:enrollment_status) { EnrollmentStatus.new }
+  subject { enrollment_status }
+  describe "Validations" do
+    describe "name" do
+      context "should be valid when" do
+        it "name is not null and is not taken" do
+          enrollment_status.name = "EnrollmentStatus name"
+          enrollment_status.should have(0).errors_on :name
+        end
+      end
+      context "should have error blank when" do
+        it "name is null" do
+          enrollment_status.name = nil
+          enrollment_status.should have_error(:blank).on :name
+        end
+      end
+      context "should have error taken when" do
+        it "name is already in use" do
+          name = "EnrollmentStatus name"
+          FactoryGirl.create(:enrollment_status, :name => name)
+          enrollment_status.name = name
+          enrollment_status.should have_error(:taken).on :name
+        end
+      end
+    end
+  end
+end
