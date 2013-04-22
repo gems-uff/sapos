19071576dbb8d787984f539a6869fcb7eba0a7d6
Bruno Schettino <bschettino@id.uff.br>
2013-04-22 19:18:36 -0300

    Phase Duration deadline validation

--- app/models/phase_duration.rb
+++ app/models/phase_duration.rb
@@ -2,14 +2,18 @@ class PhaseDuration < ActiveRecord::Base
   belongs_to :phase
   belongs_to :level
 
-  validates :deadline_semesters, :numericality => true
-  validates :deadline_months, :numericality => true
-  validates :deadline_days, :numericality => true
-
   validates :phase, :presence => true
   validates :level, :presence => true
 
+  validate :deadline_validation
+
   def to_label
     "#{deadline_semesters} períodos, #{deadline_months} meses e #{deadline_days} dias"
+  end
+
+  def deadline_validation
+    if (([0,nil].include?(self.deadline_semesters)) && ([0,nil].include?(self.deadline_months)) && ([0,nil].include?(self.deadline_days)))
+      errors.add(:deadline, I18n.t("activerecord.errors.models.phase_duration.blank_deadline"))
+    end
   end
 end
