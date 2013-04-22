d561ca146fc6fa084c48983f9300e8f3a81411e6
Bruno Schettino <bschettino@id.uff.br>
2013-04-22 19:10:07 -0300

    Major tests

---
+++ spec/models/major_spec.rb
@@ -0,0 +1,68 @@
+require "spec_helper"
+
+describe Major do
+  let(:major) { Major.new }
+  subject { major }
+  describe "Validations" do
+    describe "institution" do
+      context "should be valid when" do
+        it "institution is not null" do
+          major.institution = Institution.new
+          major.should have(0).errors_on :institution
+        end
+      end
+      context "should have error blank when" do
+        it "institution is null" do
+          major.institution = nil
+          major.should have_error(:blank).on :institution
+        end
+      end
+    end
+    describe "level" do
+      context "should be valid when" do
+        it "level is not null" do
+          major.level = Level.new
+          major.should have(0).errors_on :level
+        end
+      end
+      context "should have error blank when" do
+        it "level is null" do
+          major.level = nil
+          major.should have_error(:blank).on :level
+        end
+      end
+    end
+    describe "name" do
+      context "should be valid when" do
+        it "name is not null" do
+          major.name = "Name"
+          major.should have(0).errors_on :name
+        end
+      end
+      context "should have error blank when" do
+        it "name is null" do
+          major.name = nil
+          major.should have_error(:blank).on :name
+        end
+      end
+    end
+  end
+
+  def to_label
+    "#{name} - #{institution.name} - (#{level.name})"
+  end
+  describe "Methods" do
+    describe "to_label" do
+      it "should return the expected string" do
+        level_name = "Major Level"
+        major_name = "Major Name"
+        institution_name = "Major Institution"
+        major.level = Level.new(:name => level_name)
+        major.name = major_name
+        major.institution = Institution.new(:name => institution_name)
+        expected = "#{major_name} - #{institution_name} - (#{level_name})"
+        major.to_label.should eql(expected)
+      end
+    end
+  end
+end
