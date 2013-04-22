725a9ebb2faddfdc2e6d3c0a632d45096861b6a3
Bruno Schettino <bschettino@id.uff.br>
2013-04-22 19:36:04 -0300

    Student tests

---
+++ spec/models/student_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+
+describe Student do
+  let(:student) { Student.new }
+  subject { student }
+  describe "Validations" do
+    describe "cpf" do
+      context "should be valid when" do
+        it "cpf is not null and is not taken" do
+          student.cpf = "Student cpf"
+          student.should have(0).errors_on :cpf
+        end
+      end
+      context "should have error blank when" do
+        it "cpf is null" do
+          student.cpf = nil
+          student.should have_error(:blank).on :cpf
+        end
+      end
+      context "should have error taken when" do
+        it "cpf is already in use" do
+          cpf = "Student cpf"
+          FactoryGirl.create(:student, :cpf => cpf)
+          student.cpf = cpf
+          student.should have_error(:taken).on :cpf
+        end
+      end
+    end
+    describe "name" do
+      context "should be valid when" do
+        it "name is not null and is not taken" do
+          student.name = "Student name"
+          student.should have(0).errors_on :name
+        end
+      end
+      context "should have error blank when" do
+        it "name is null" do
+          student.name = nil
+          student.should have_error(:blank).on :name
+        end
+      end
+      end
+  end
+end
