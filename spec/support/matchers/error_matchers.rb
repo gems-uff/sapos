ff1c08e49dab0d3eff934ebc471cf6b7fb48dbae
Bruno Schettino <bschettino@id.uff.br>
2013-04-23 10:20:52 -0300

    Add new error matcher

--- spec/support/matchers/error_matchers.rb
+++ spec/support/matchers/error_matchers.rb
@@ -4,8 +4,11 @@ RSpec::Matchers.define :have_error do |erro|
   def error_message(record, atributo, erro)
     translation_missing_message = "translation missing:"
     message = I18n.translate("errors.messages.#{erro}")
-
-    return message.include?(translation_missing_message) ? I18n.translate("activerecord.errors.models.#{record.class.to_s.underscore}.#{erro}") : message
+    if message.include?(translation_missing_message)
+      message = I18n.translate("activerecord.errors.models.#{record.class.to_s.underscore}.#{erro}")
+      message = message.include?(translation_missing_message) ? I18n.translate("activerecord.errors.models.#{record.class.to_s.underscore}.attributes.#{atributo}.#{erro}") : message
+    end
+    return message
   end
 
   chain :on do |atributo|
