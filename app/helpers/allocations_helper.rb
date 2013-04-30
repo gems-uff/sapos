273ad23d6d7b996aeef319582ced02d725d0d20f
Bruno Schettino <bschettino@id.uff.br>
2013-04-29 22:57:05 -0300

    Remove timepicker from allocation views

--- app/helpers/allocations_helper.rb
+++ app/helpers/allocations_helper.rb
@@ -1,12 +1,3 @@
 module AllocationsHelper
-  def start_time_form_column(record, options)
-    options[:class] += " time_picker"
-    text_field :record, :start_time, {
-    }.merge(options)
-  end
-  def end_time_form_column(record, options)
-    options[:class] += " time_picker"
-    text_field :record, :start_time, {
-    }.merge(options)
-  end
+
 end
