cfba283b80ac0c66a353a4d978b66d1a06bcb0a0
Everton Moreth <everton.moreth@gmail.com>
2013-05-18 23:33:18 -0300

    Correcao de layout de login
    Correcao no helper de exibicao para usuarios logados.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def display_none_if_logged_out
-    "style='display:none;'" unless user_signed_in?
+    'style="display:none;"'.html_safe unless user_signed_in?
   end
 
   def transparent_if_logged_out
-    "style='background: transparent;border: 0;'" unless user_signed_in?
+    'style="background: transparent;border: 0;"'.html_safe unless user_signed_in?
   end
 end
