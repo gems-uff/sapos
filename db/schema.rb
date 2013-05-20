f6a4b32bfcdde1c554f97c55aa99686ca093f05a
Everton Moreth <everton.moreth@gmail.com>
2013-05-20 16:32:08 -0300

    Atualizando roles de secretaria

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130508231824) do
+ActiveRecord::Schema.define(:version => 20130520190910) do
 
   create_table "accomplishments", :force => true do |t|
     t.integer  "enrollment_id"
