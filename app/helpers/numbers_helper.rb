52520c94cff23e75c52f582bd767aac41a0f77e7
Bruno Schettino <bschettino@id.uff.br>
2013-05-22 18:29:07 -0300

    Fix number_with_delimiter override

--- app/helpers/numbers_helper.rb
+++ app/helpers/numbers_helper.rb
@@ -1,32 +1,50 @@
 module NumbersHelper
   class InvalidNumberError < StandardError
     attr_accessor :number
+
     def initialize(number)
       @number = number
     end
   end
 
   def number_with_delimiter(number, options = {})
-    return nil if number.nil?
-
     options.symbolize_keys!
 
-    defaults = I18n.translate(:'number.format', :locale => options[:locale], :default => {})
-    formated_number = I18n.translate(:"number.#{options[:format_as]}.format", :locale => options[:locale], :default => {})
-    defaults = defaults.merge(formated_number)
+    if options[:format_as]
+      return nil if number.nil?
+      defaults = I18n.translate(:'number.format', :locale => options[:locale], :default => {})
+      formated_number = I18n.translate(:"number.#{options[:format_as]}.format", :locale => options[:locale], :default => {})
+      defaults = defaults.merge(formated_number)
 
-    options = options.reverse_merge(defaults)
+      options = options.reverse_merge(defaults)
 
-    begin
-      send("number_to_#{options[:format_as]}", number, options || {}).html_safe
-    rescue InvalidNumberError => e
-      if options[:raise]
-        raise
-      else
-        e.number.to_s.html_safe? ? "#{e.number}".html_safe : "#{e.number}"
+      begin
+        send("number_to_#{options[:format_as]}", number, options || {}).html_safe
+      rescue InvalidNumberError => e
+        if options[:raise]
+          raise
+        else
+          e.number.to_s.html_safe? ? "#{e.number}".html_safe : "#{e.number}"
+        end
       end
-    end
+    else
+      begin
+        Float(number)
+      rescue ArgumentError, TypeError
+        if options[:raise]
+          raise InvalidNumberError, number
+        else
+          return number
+        end
+      end
 
+      defaults = I18n.translate(:'number.format', :locale => options[:locale], :default => {})
+      options = options.reverse_merge(defaults)
+
+      parts = number.to_s.to_str.split('.')
+      parts[0].gsub!(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1#{options[:delimiter]}")
+      parts.join(options[:separator]).html_safe
+    end
   end
 
   def number_to_grade(number, options = {})
