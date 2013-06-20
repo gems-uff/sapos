6916ed8e82933d6a4487e549b818b2022ee4ec6d
Everton Moreth <everton.moreth@gmail.com>
2013-06-20 20:28:34 -0300

    Adicionando encoding em controllers

--- app/controllers/scholarship_durations_controller.rb
+++ app/controllers/scholarship_durations_controller.rb
@@ -1,7 +1,7 @@
+# encoding: utf-8
 # Copyright (c) 2013 Universidade Federal Fluminense (UFF).
 # This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
 
-# encoding: utf-8
 class ScholarshipDurationsController < ApplicationController
   active_scaffold :scholarship_duration do |config|
     config.action_links.add 'to_pdf', :label => I18n.t('active_scaffold.to_pdf'), :page => true, :type => :collection
