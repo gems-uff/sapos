a0a38d7631182cc91b0346d813e7a24dfd73cbed
Everton Moreth <everton.moreth@gmail.com>
2013-06-28 17:25:47 -0300

    Configuracao de asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Sapos::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "https://sel.ic.uff.br"
+  config.action_controller.asset_host = "https://sel.ic.uff.br/sapos"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
