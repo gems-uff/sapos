0f6a6f76defff442fdeae7235ab1fb13dbb310ce
Joao Felipe <joaofelipenp@gmail.com>
2013-09-24 09:58:45 -0300

    fixing error while creating research area

--- app/models/research_area.rb
+++ app/models/research_area.rb
@@ -4,9 +4,12 @@
 class ResearchArea < ActiveRecord::Base
   has_many :courses
   has_many :professors, :through => :professor_research_areas
+  has_many :professor_research_areas
 
   validates :name, :presence => true, :uniqueness => true
   validates :code, :presence => true, :uniqueness => true
+
+  attr_accessible :name, :code
 
   def to_label
     "#{code} - #{name}"
