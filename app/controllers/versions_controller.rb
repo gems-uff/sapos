6760d809af32a7556675802f783e21af0141bada
Luis Vieira <luis.vieira@myfreecomm.com.br>
2013-10-08 10:40:10 -0300

    Adicionando links para os objetos, e traduções

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -6,15 +6,16 @@ class VersionsController < ApplicationController
   authorize_resource
 
   active_scaffold :version do |config|
-    config.columns = [:item_type, :item_id, :event, :whodunnit]
+    config.columns = [:item_type, :current_object, :event, :user]
+    config.list.sorting = {:created_at => 'DESC'}
+    config.search.columns = [:user, :event, :item_type, :item_id]
 
     config.columns[:item_type].label = I18n.t('activerecord.attributes.version.item_type')
-    config.columns[:item_id].label = I18n.t('activerecord.attributes.version.item_id')
+    config.columns[:current_object].label = I18n.t('activerecord.attributes.version.current_object')
     config.columns[:event].label = I18n.t('activerecord.attributes.version.event')
-    config.columns[:whodunnit].label = I18n.t('activerecord.attributes.version.whodunnit')
+    config.columns[:user].label = I18n.t('activerecord.attributes.version.user')
 
     config.actions.exclude :create, :delete, :update
   end
-  record_select :per_page => 10, :order_by => 'created_at', :search_on => [:item_id]
 
  end
--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -3,7 +3,30 @@
 # This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
 
 module VersionsHelper
-	def whodunnit_column(record, column)
+	def item_type_column(record, column)
+		I18n.t("activerecord.models.#{record.item_type.downcase}.one")
+	end
+
+	def current_object_column(record, column)
+		object_id = record.item_id.to_i
+		model_str = record.item_type
+		model = model_str.constantize
+
+		if model.find_by_id(object_id).nil?
+			I18n.t("activerecord.attributes.version.current_object_destroyed")
+		else
+			path = send((model_str.downcase + "_path").to_sym, object_id)
+			route = Rails.application.routes.recognize_path(path)
+
+			link_to(h(model.find(object_id).to_label), route) if object_id != 0
+		end
+	end
+
+	def event_column(record, column)
+		I18n.t("activerecord.attributes.version.event_options.#{record.event}")
+	end
+
+	def user_column(record, column)
 		user_id = record.whodunnit.to_i
 		link_to(h(User.find(user_id).name), user_path(User.find(user_id))) if user_id != 0
 	end
