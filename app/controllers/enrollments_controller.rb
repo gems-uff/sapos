438a8f0447c55ac5265a29c1cf5de18c65b3bef3
Bernardo Santos <bernardo.csantos@gmail.com>
2013-10-16 20:02:07 -0300

    Merging master with the feature

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -8,8 +8,6 @@ class EnrollmentsController < ApplicationController
   include NumbersHelper
   include ApplicationHelper
   helper :class_enrollments
-  helper :advisements
-  helper :scholarship_durations
 
   active_scaffold :enrollment do |config|
 
@@ -25,6 +23,8 @@ class EnrollmentsController < ApplicationController
     config.columns[:accomplishments].allow_add_existing = false;
 
     config.columns.add :scholarship_durations_active, :active, :professor, :phase, :delayed_phase, :course_class_year_semester
+    config.columns.add :listed_advisors, :listed_accomplishments, :listed_deferrals, :listed_scholarships, :listed_class_enrollments
+
     config.actions.swap :search, :field_search
     config.field_search.columns = [:enrollment_number, :student, :level, :enrollment_status, :admission_date, :active, :scholarship_durations_active, :professor, :accomplishments, :delayed_phase, :course_class_year_semester]
 
@@ -66,7 +66,8 @@ class EnrollmentsController < ApplicationController
     config.columns[:student].form_ui = :record_select
     config.create.columns = [:enrollment_number, :admission_date, :level, :enrollment_status, :obs, :student, :thesis_title, :advisements, :accomplishments, :deferrals, :scholarship_durations, :dismissal, :class_enrollments]
     config.update.columns = [:enrollment_number, :admission_date, :level, :enrollment_status, :obs, :student, :thesis_title, :advisements, :accomplishments, :deferrals, :scholarship_durations, :dismissal, :class_enrollments]
-    config.show.columns = [:enrollment_number, :admission_date, :level, :enrollment_status, :obs, :student, :advisements, :accomplishments, :deferrals, :scholarship_durations, :dismissal, :class_enrollments]
+    config.show.columns = [:enrollment_number, :admission_date, :level, :enrollment_status, :obs, :student, :listed_advisors, :listed_accomplishments, :listed_deferrals, :listed_scholarships, :dismissal, :listed_class_enrollments]
+
   end
   record_select :per_page => 10, :search_on => [:enrollment_number], :order_by => 'enrollment_number', :full_text_search => true
 
@@ -213,6 +214,5 @@ class EnrollmentsController < ApplicationController
       end
     end
   end
-
 
 end
