65b30ea73b92f263a565335f0cfb37c28d852948
Luis Vieira <luis.vieira@myfreecomm.com.br>
2013-10-28 21:18:23 -0200

    issue #9 e issue #76 movendo lógica do helper para o model e criando testes

--- app/models/deferral.rb
+++ app/models/deferral.rb
@@ -15,4 +15,43 @@ class Deferral < ActiveRecord::Base
     "#{deferral_type.name}" unless deferral_type.nil?    
   end
 
+  def valid_until
+    admission_date = enrollment.admission_date
+
+    durations = deferral_type.phase.phase_durations
+    phase_duration = durations.select { |duration| duration.level_id == enrollment.level.id}[0]
+
+    phase_duration_semesters = phase_duration.deadline_semesters
+    phase_duration_months = phase_duration.deadline_months
+    phase_duration_days = phase_duration.deadline_days
+
+    deferral_duration_semesters = deferral_type.duration_semesters
+    deferral_duration_months = deferral_type.duration_months
+    deferral_duration_days = deferral_type.duration_days
+
+    total_duration_semesters = phase_duration_semesters + deferral_duration_semesters
+    total_duration_months = phase_duration_months + deferral_duration_months
+    total_duration_days = phase_duration_days + deferral_duration_days
+
+    valid_date = admission_date
+
+    if total_duration_semesters != 0
+      valid_date = (12 * (total_duration_semesters / 2)).months.since(valid_date)
+      valid_date = valid_date.month == 3 ? 
+        (5 * (total_duration_semesters % 2)).months.since(valid_date) : 
+        (7 * (total_duration_semesters % 2)).months.since(valid_date)
+      valid_date = valid_date - 1
+    end
+
+    if total_duration_months != 0
+      valid_date = total_duration_months.months.since(valid_date).end_of_month
+    end
+
+    if total_duration_days != 0
+      valid_date = total_duration_days.days.since(valid_date)
+    end
+
+    valid_date.strftime('%d/%m/%Y')
+  end
+
 end
--- spec/models/deferral_spec.rb
+++ spec/models/deferral_spec.rb
@@ -44,5 +44,91 @@ describe Deferral do
         deferral.to_label.should eql(deferral_type_name)
       end
     end
+    describe "valid_until" do
+      it "should return 31th of july 2013" do
+        enrollment = Enrollment.new()
+        level = Level.new
+        phase = Phase.new
+        phase_duration = PhaseDuration.new
+        deferral_type = DeferralType.new
+
+        level.id = 1
+        phase_duration.deadline_semesters = 1
+        phase_duration.level_id = level.id
+
+        enrollment.admission_date = Date.new(2012, 3, 1)
+        enrollment.level = level
+        
+        phase.levels = [level]
+        phase.phase_durations = [phase_duration]
+        phase.enrollments = [enrollment]
+
+        deferral_type.phase = phase
+        deferral_type.duration_semesters = 2
+
+        deferral.enrollment = enrollment
+        deferral.deferral_type = deferral_type
+
+        puts deferral.valid_until
+        deferral.valid_until.should eql(Date.new(2013, 7, 31).strftime('%d/%m/%Y'))
+      end
+
+      it "should return 28th of february 2023" do
+        enrollment = Enrollment.new()
+        level = Level.new
+        phase = Phase.new
+        phase_duration = PhaseDuration.new
+        deferral_type = DeferralType.new
+
+        level.id = 1
+        phase_duration.deadline_semesters = 8
+        phase_duration.level_id = level.id
+
+        enrollment.admission_date = Date.new(2017, 8, 1)
+        enrollment.level = level
+        
+        phase.levels = [level]
+        phase.phase_durations = [phase_duration]
+        phase.enrollments = [enrollment]
+
+        deferral_type.phase = phase
+        deferral_type.duration_semesters = 3
+
+        deferral.enrollment = enrollment
+        deferral.deferral_type = deferral_type
+
+        puts deferral.valid_until
+        deferral.valid_until.should eql(Date.new(2023, 2, 28).strftime('%d/%m/%Y'))
+      end
+
+      it "should return 7th of june 2014" do
+        enrollment = Enrollment.new()
+        level = Level.new
+        phase = Phase.new
+        phase_duration = PhaseDuration.new
+        deferral_type = DeferralType.new
+
+        level.id = 1
+        phase_duration.deadline_semesters = 1
+        phase_duration.level_id = level.id
+
+        enrollment.admission_date = Date.new(2013, 8, 1)
+        enrollment.level = level
+        
+        phase.levels = [level]
+        phase.phase_durations = [phase_duration]
+        phase.enrollments = [enrollment]
+
+        deferral_type.phase = phase
+        deferral_type.duration_months = 3
+        deferral_type.duration_days = 7
+
+        deferral.enrollment = enrollment
+        deferral.deferral_type = deferral_type
+
+        puts deferral.valid_until
+        deferral.valid_until.should eql(Date.new(2014, 6, 7).strftime('%d/%m/%Y'))
+      end
+    end
   end
 end
