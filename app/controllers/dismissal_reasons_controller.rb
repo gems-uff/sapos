760eca7b3aee60b1cd7489ab44df5a91cd4f33a4
JoaoFelipe <joaofelipenp@gmail.com>
2014-01-15 20:58:47 -0200

    Issue #89: Moving thesis_judgement to the last column of DismissalReason visualization

--- app/controllers/dismissal_reasons_controller.rb
+++ app/controllers/dismissal_reasons_controller.rb
@@ -10,7 +10,7 @@ class DismissalReasonsController < ApplicationController
     config.create.label = :create_dismissal_reason_label
   	config.columns[:thesis_judgement].form_ui = :select
     config.columns[:thesis_judgement].options = {:options => DismissalReason::THESIS_JUDGEMENT, :include_blank => I18n.t("active_scaffold._select_")}
-
+    config.columns = [:name, :description, :show_advisor_name, :thesis_judgement]
   end
 
 end 
