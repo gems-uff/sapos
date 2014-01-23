b8fdd9557499f2da6470bae1c63b676fa9e07aed
JoaoFelipe <joaofelipenp@gmail.com>
2014-01-23 14:11:35 -0200

    Issue #98: using just name and advisement_authorizations columns for levels

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -8,8 +8,8 @@ class LevelsController < ApplicationController
   helper :advisement_authorizations
   active_scaffold :level do |config|
     config.list.sorting = {:name => 'ASC'}
-    config.update.columns = [:name, :advisement_authorizations]
-    config.create.columns = [:name, :advisement_authorizations]
-    #config.create.label = :create_level_label
+    columns = [:name, :advisement_authorizations]
+    config.columns = columns
+    config.create.label = :create_level_label
   end
 end 
