bfd2358dae17c979b10411ad643d176f90a1c2a2
JoaoFelipe <joaofelipenp@gmail.com>
2014-01-25 17:08:36 -0200

    Issue #12: controllers

--- app/controllers/notification_logs_controller.rb
+++ app/controllers/notification_logs_controller.rb
@@ -1,6 +1,7 @@
 class NotificationLogsController < ApplicationController
   authorize_resource
-  
+
   active_scaffold :"notification_log" do |config|
+  	config.actions.exclude :create, :delete, :update
   end
 end
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,13 @@
 class NotificationsController < ApplicationController
   authorize_resource
-  
-  active_scaffold :"notification" do |conf|
+
+  active_scaffold :"notification" do |config|
+  	form_columns = [:frequency, :notification_offset, :query_offset, :sql_query, :to_template, :subject_template, :body_template]
+    config.update.columns = form_columns
+    config.create.columns = form_columns
+    config.show.columns = form_columns + [:last_execution]
+    
+    config.list.columns = [:last_execution]
+    config.create.label = :create_notification_label
   end
 end
