4b0f1f3299e2fad863398c8f62dbfcf0dee2a51d
Luis Vieira <lvieira@lvieira.com>
2014-01-25 20:59:47 -0200

    Issue #12: removendo migrations irreversiveis e alterando notificacao padrao

--- db/migrate/20140125183203_add_last_execution_to_notification.rb
+++
@@ -1,5 +0,0 @@
-class AddLastExecutionToNotification < ActiveRecord::Migration
-  def change
-    add_column :notifications, :last_execution, :datetime
-  end
-end
--- db/migrate/20140125185989_add_body_to_notification.rb
+++
@@ -1,5 +0,0 @@
-class AddBodyToNotification < ActiveRecord::Migration
-  def change
-    change_column :notifications, :body_template, :text, :limit => nil
-  end
-end
