fb60cc4bd8625b6668593e53cd5b44034d1f7e90
Luis Vieira <lvieira@lvieira.com>
2014-01-25 22:15:29 -0200

    Issue #12: alteracoes nos controllers

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,12 +5,12 @@ class NotificationsController < ApplicationController
   	form_columns = [:title, :frequency, :notification_offset, :query_offset, :sql_query, :to_template, :subject_template, :body_template]
     config.update.columns = form_columns
     config.create.columns = form_columns
-    config.show.columns = form_columns + [:last_execution]
+    config.show.columns = form_columns + [:next_execution]
 
     config.columns[:frequency].form_ui = :select
     config.columns[:frequency].options = {:options => Notification::FREQUENCIES}
     
-    config.list.columns = [:title, :frequency, :notification_offset, :query_offset, :last_execution]
+    config.list.columns = [:title, :frequency, :notification_offset, :query_offset, :next_execution]
     config.create.label = :create_notification_label
   end
 end
