6ddfd61f719502e9bf2aeb06bb129c17892245c8
JoaoFelipe <joaofelipenp@gmail.com>
2014-01-26 13:23:43 -0200

    Issue #12: notification show

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -22,4 +22,27 @@ module NotificationsHelper
   def body_template_form_column(record, options)
     code_mirror_text_area(:body_template, "record_body_template_#{record.id}", "text/html", options.merge(:value => record.body_template || I18n.t('active_scaffold.notification.body_template_default')))
   end
+
+  def next_execution_column(record, options)
+    I18n.localize(record.next_execution, {:format => :day}) unless record.next_execution.nil? or record.nil?
+  end
+
+  def sql_query_show_column(record, column)
+    type = "text/x-mysql"
+    value = record.sql_query
+    id = "sql_query-view-#{record.id}"
+    "<div id='sql_query-view-#{record.id}'></div>
+    <script>
+    CodeMirror(document.getElementById('#{id}'),
+     {mode: '#{type}',
+      value: '#{value}',
+      indentWithTabs: true,
+      smartIndent: true,
+      lineNumbers: true,
+      matchBrackets : true,
+      autofocus: true
+     }
+    );
+    </script>".html_safe
+  end
 end
