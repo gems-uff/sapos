6a8aaabdcd1b38587e574036545b36c58df12894
JoaoFelipe <joaofelipenp@gmail.com>
2014-01-25 23:03:51 -0200

    Issue #12: code highlight for query

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,7 +1,17 @@
 module NotificationsHelper
 
   def sql_query_form_column(record, options)
-    block = text_field(:record, :sql_query, options)
-
+    block = text_area(:record, :sql_query, options.merge(:value => record.sql_query || "SELECT * FROM..."))
+    block += "<script>
+    CodeMirror.fromTextArea(document.getElementById('record_sql_query_#{record.id}'),
+     {mode: 'text/x-mysql',
+      indentWithTabs: true,
+      smartIndent: true,
+      lineNumbers: true,
+      matchBrackets : true,
+      autofocus: true
+     }
+    );
+    </script>".html_safe
   end
 end
