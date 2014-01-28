bfe75c30ca93a569d9988b39b431a2855175fca4
Luis Vieira <lvieira@lvieira.com>
2014-01-28 10:17:39 -0200

    Issur #12: ordenando desc por data

--- app/controllers/notification_logs_controller.rb
+++ app/controllers/notification_logs_controller.rb
@@ -4,5 +4,7 @@ class NotificationLogsController < ApplicationController
   active_scaffold :"notification_log" do |config|
   	config.columns = [:notification, :to, :subject, :body, :created_at]
   	config.actions.exclude :create, :delete, :update
+
+  	config.list.sorting = {:created_at => 'DESC'}
   end
 end
