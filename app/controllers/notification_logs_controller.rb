a982e7b42e89c27076de2e8092c742952050e7b6
Luis Vieira <lvieira@lvieira.com>
2014-01-29 23:38:10 -0200

    Issue #12: alterando para mostrar Notificacao removida, na janela de logs das notificacoes quando a notificacao qeu foi enviada for deletada do banco

--- app/controllers/notification_logs_controller.rb
+++ app/controllers/notification_logs_controller.rb
@@ -1,10 +1,17 @@
+# encoding utf-8
+# Copyright (c) 2013 Universidade Federal Fluminense (UFF).
+# This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
+
 class NotificationLogsController < ApplicationController
   authorize_resource
 
   active_scaffold :"notification_log" do |config|
-  	config.columns = [:notification, :to, :subject, :body, :created_at]
+  	config.columns = [:notification_name, :to, :subject, :body, :created_at]
+  	config.show.columns = [:notification_name, :to, :subject, :body, :created_at]
+  	config.columns[:notification_name].label = I18n.t('activerecord.attributes.notification_log.notification_name')
+  	config.list.sorting = {:created_at => 'DESC'}
   	config.actions.exclude :create, :delete, :update
 
-  	config.list.sorting = {:created_at => 'DESC'}
+  	
   end
 end
