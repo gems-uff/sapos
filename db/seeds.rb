6a3d34da2e21135a1be8ce5a1b45050c2a9211ce
JoaoFelipe <joaofelipenp@gmail.com>
2014-02-07 23:07:46 -0200

    Issue #107: changing notification_start_at to 4:00

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ CustomVariable.create(:name=>"Nível do Programa na CAPES", :variable =>"program
 CustomVariable.create(:name=>"País padrão de emissão da identidade", :variable =>"identity_issuing_country", :value => "Brasil")
 CustomVariable.create(:name=>"Texto no final do quadro de horários", :variable =>"class_schedule_text", :value => "Alunos interessados em cursar disciplinas de Tópicos Avançados devem consultar os respectivos professores antes da matrícula.")
 CustomVariable.create(:name=>"Frequência de envio de notificações (reiniciar servidor após alteração)", :variable =>"notification_frequency", :value => "1d")
-CustomVariable.create(:name=>"Horário inicial para o envio de notificações (reiniciar servidor após alteração)", :variable =>"notification_start_at", :value => "12:00")
+CustomVariable.create(:name=>"Horário inicial para o envio de notificações (reiniciar servidor após alteração)", :variable =>"notification_start_at", :value => "4:00")
 CustomVariable.create(:name=>"Texto no final do quadro de horários", :variable =>"class_schedule_text", :value => "Alunos interessados em cursar disciplinas de Tópicos Avançados devem consultar os respectivos professores antes da matrícula")
 CustomVariable.create(:name=>"E-mail de redirecionamento para as notificações", :variable =>"redirect_email", :value => "")
 CustomVariable.create(:name=>"Texto de rodapé da notificação", :variable =>"notification_footer", :value => "Pós-Graduação em Computação - IC/UFF
