fa5d903f30ae25de1e9fefe9f07fbc8c9bf08161
JoaoFelipe <joaofelipenp@gmail.com>
2014-02-22 13:08:07 -0300

    Trying to fix migration

--- db/migrate/20140207153940_populate_professor_email.rb
+++ db/migrate/20140207153940_populate_professor_email.rb
@@ -2,13 +2,6 @@ class PopulateProfessorEmail < ActiveRecord::Migration
   def up
     add_index :users, :email
     add_index :professors, :email
-    User.all.each do |user| 
-      professor = Professor.where(:name => user.name).first
-      unless professor.nil?
-        professor.email = user.email
-        professor.save
-      end
-    end
   end
 
   def down
