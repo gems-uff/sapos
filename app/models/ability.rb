1402aa96f278f293de8ec0c4881ba5fedcd242b8
JoaoFelipe <joaofelipenp@gmail.com>
2014-02-24 14:55:16 -0300

    Issue #117: fixing professor ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,16 +6,21 @@ class Ability
 
   ALL_MODELS = [Accomplishment, Advisement, AdvisementAuthorization, Allocation,
                 City, ClassEnrollment, Country, Course, CourseClass, CourseType,
-                CustomVariable, Deferral, DeferralType, Dismissal, DismissalReason, Enrollment,
-                EnrollmentStatus, Institution, Level, Major, Notification, NotificationLog,
-                Phase, PhaseCompletion, PhaseDuration, Professor, ProfessorResearchArea, ResearchArea, 
-                Role, Scholarship, ScholarshipDuration, ScholarshipType, Sponsor, State, Student, 
-                StudentMajor, ThesisDefenseCommitteeParticipation, User, YearSemester, Version]
+                CustomVariable, Deferral, DeferralType, Dismissal, 
+                DismissalReason, Enrollment, EnrollmentStatus, Institution,
+                Level, Major, Notification, NotificationLog, Phase, 
+                PhaseCompletion, PhaseDuration, Professor, ProfessorResearchArea,
+                ResearchArea, Role, Scholarship, ScholarshipDuration, 
+                ScholarshipType, Sponsor, State, Student, StudentMajor, 
+                ThesisDefenseCommitteeParticipation, User, Version, YearSemester] 
 
 
   def initialize(user)
-    alias_action :list, :row, :show_search, :render_field, :to_pdf, :summary_pdf, :academic_transcript_pdf, :grades_report_pdf, :browse, :to => :read
-    alias_action :update_column, :edit_associated, :new_existing, :add_existing, :execute_now, :to => :update
+    alias_action :list, :row, :show_search, :render_field, :class_schedule_pdf,
+        :to_pdf, :summary_pdf, :academic_transcript_pdf, :grades_report_pdf, 
+        :browse, :simulate, :set_query_date, :cities, :states, :to => :read
+    alias_action :update_column, :edit_associated, :new_existing, :add_existing, 
+        :execute_now, :execute_now, :notify, :to => :update
     alias_action :delete, :destroy_existing, :to => :destroy
     #as_action_aliases
 
