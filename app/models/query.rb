d94c5ccebe88ac4cc3cc377b4f8fcaaf490ca94b
Everton Moreth <everton.moreth@gmail.com>
2014-04-29 00:20:19 -0300

    First attempt to make query update on params change

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,3 +1,6 @@
+# Copyright (c) 2013 Universidade Federal Fluminense (UFF).
+# This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
+
 class Query < ActiveRecord::Base
 
   has_many :notifications, inverse_of: :query
@@ -7,21 +10,31 @@ class Query < ActiveRecord::Base
 
   accepts_nested_attributes_for :params, reject_if: :all_blank
 
-
-  def try_field(field, &block)
-    begin
-      return yield
-    rescue Exception => e
-      raise [field, e.to_s].join(' -:- ')
-    end
-  end
+  validate :ensure_valid_params
 
   def map_params
     current_params = {}
     self.params.each do |param|
-      current_params[param.name.to_sym] = param.default_value
+      case param.value_type
+        when 'Date'
+          current_params[param.name.to_sym] = Date.parse(param.default_value)
+        when 'DateTime'
+          current_params[param.name.to_sym] = DateTime.parse(param.default_value)
+        when 'List'
+          current_params[param.name.to_sym] = param.default_value.split(',')
+        when 'Number'
+          current_params[param.name.to_sym] = param.default_value.to_f
+        when 'Literal'
+          current_params[param.name.to_sym] = param.default_value
+        else
+          current_params[param.name.to_sym] = param.default_value
+      end
     end
     current_params
+  end
+
+  def self.parse_sql_and_params(sql, params)
+    self.send(:sanitize_sql, [sql, params])
   end
 
   def execute(options={})
@@ -31,10 +44,23 @@ class Query < ActiveRecord::Base
     #Generate query using the parameters specified by the notification
     current_params = map_params
 
+    generated_query = ::Query.parse_sql_and_params(sql, current_params)
+
     #Query the Database
-    db_resource = db_connection.exec_query(sql, current_params)
+    db_resource = db_connection.exec_query(generated_query)
 
     #Build the notifications with the results from the query
-    {:columns => db_resource.columns, :rows => db_resource.rows}
+    {:columns => db_resource.columns, :rows => db_resource.rows, :query => generated_query}
+  end
+
+
+  def ensure_valid_params
+    begin
+      self.execute
+    rescue ActiveRecord::PreparedStatementInvalid => e
+      variable_name = e.message.match(/missing value for :([a-zA-Z0-9_]+)/)[1]
+
+      self.errors.add(:base, "O parametro #{variable_name} foi usado na query mas nao foi definido.")
+    end
   end
 end
--- app/models/query_param.rb
+++ app/models/query_param.rb
@@ -1,9 +1,12 @@
+# Copyright (c) 2013 Universidade Federal Fluminense (UFF).
+# This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
+
 class QueryParam < ActiveRecord::Base
   belongs_to :query
   attr_accessible :name, :default_value, :value_type
 
 
-  VALUE_TYPES = %w{String Number List Date Literal}
+  VALUE_TYPES = %w{String Number List Date DateTime Literal}
 
   validates :value_type, presence: true, inclusion: VALUE_TYPES
   validates :name, presence: true, :format => /^([a-z_][a-zA-Z_0-9]+)?$/
