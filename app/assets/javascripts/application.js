d4fcfe25779448d8b0d39534edd25777ae6ed316
Everton Moreth <everton.moreth@gmail.com>
2014-05-04 23:41:35 -0300

    Fix on param parsing function. #130

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,46 +24,80 @@ var I18nExitConfirmation = 'Existem campos preenchidos! Você pode perder suas a
 
 
 function areInputsFilled(selector) {
-	var filled = false;
-	$(selector).each(function() {
-	   var element = $(this);
-	   if (element.val() != "") {
-	       filled = true;
-	   }
-	});
-	return filled;
+  var filled = false;
+  $(selector).each(function () {
+    var element = $(this);
+    if (element.val() != "") {
+      filled = true;
+    }
+  });
+  return filled;
 }
 
 function confirmOnPageExit() {
-	if (areInputsFilled('.as_form.update input[type=text], .as_form.create input[type=text]')) {
-		return I18nExitConfirmation;
-	}
+  if (areInputsFilled('.as_form.update input[type=text], .as_form.create input[type=text]')) {
+    return I18nExitConfirmation;
+  }
 }
 
-function prevSemester(date){
-    return new Date(date.getTime() - DAY_IN_MILLISECONDS * 180);
+function prevSemester(date) {
+  return new Date(date.getTime() - DAY_IN_MILLISECONDS * 180);
 }
 
-function nextSemester(date){
-    return new Date(date.getTime() + DAY_IN_MILLISECONDS * 180);
+function nextSemester(date) {
+  return new Date(date.getTime() + DAY_IN_MILLISECONDS * 180);
 }
 
 function addToQueryString(url, key, value) {
-    var query = url.indexOf('?');
-    var anchor = url.indexOf('#');
-    if (query == url.length - 1) {
-        // Strip any ? on the end of the URL
-        url = url.substring(0, query);
-        query = -1;
-    }
-    return (anchor > 0 ? url.substring(0, anchor) : url)
-         + (query > 0 ? "&" + key + "=" + value : "?" + key + "=" + value)
-         + (anchor > 0 ? url.substring(anchor) : "");
+  var query = url.indexOf('?');
+  var anchor = url.indexOf('#');
+  if (query == url.length - 1) {
+    // Strip any ? on the end of the URL
+    url = url.substring(0, query);
+    query = -1;
+  }
+  return (anchor > 0 ? url.substring(0, anchor) : url)
+    + (query > 0 ? "&" + key + "=" + value : "?" + key + "=" + value)
+    + (anchor > 0 ? url.substring(anchor) : "");
 }
 
-$(document).ready(function(){
-	window.onbeforeunload = confirmOnPageExit;
-	cityWidget();
-	identityIssuingPlaceWidget();
+
+function queryStringToHash(query) {
+
+  if (query == '') return null;
+
+  var hash = {};
+
+  var vars = query.split("&");
+
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    var k = decodeURIComponent(pair[0]);
+    var v = decodeURIComponent(pair[1]);
+
+    // If it is the first entry with this name
+    if (typeof hash[k] === "undefined") {
+
+      if (k.substr(k.length - 2) != '[]')  // not end with []. cannot use negative index as IE doesn't understand it
+        hash[k] = v;
+      else
+        hash[k] = [v];
+
+      // If subsequent entry with this name and not array
+    } else if (typeof hash[k] === "string") {
+      hash[k] = v;  // replace it
+
+      // If subsequent entry with this name and is array
+    } else {
+      hash[k].push(v);
+    }
+  }
+  return hash;
+};
+
+$(document).ready(function () {
+  window.onbeforeunload = confirmOnPageExit;
+  cityWidget();
+  identityIssuingPlaceWidget();
 });
 
