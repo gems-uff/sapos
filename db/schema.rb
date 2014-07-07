6a2a72bdac5cbae07617a2502aa1a4b117219057
JoaoFelipe <joaofelipenp@gmail.com>
2014-07-07 20:50:53 -0300

    Removing require debugger

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20140707232936) do
+ActiveRecord::Schema.define(:version => 20140707232935) do
 
   create_table "accomplishments", :force => true do |t|
     t.integer  "enrollment_id"
