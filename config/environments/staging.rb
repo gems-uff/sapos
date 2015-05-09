eb58d3654c66de1cc3ea1082535d5dad3d2be3c4
Everton Moreth <everton.moreth@gmail.com>
2015-05-09 17:35:41 -0300

    Using digests for assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -35,7 +35,7 @@ Sapos::Application.configure do
   config.assets.compile = true
 
   # Generate digests for assets URLs.
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
