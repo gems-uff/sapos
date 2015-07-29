d7f99c513ca2241ae0f9649854972598afb7c5c8
Joao Felipe <joaofelipenp@gmail.com>
2015-07-28 22:41:15 -0300

    #164: extend fix to any sql adapter

--- config/initializers/recordselect_patch.rb
+++ config/initializers/recordselect_patch.rb
@@ -1,9 +1,8 @@
 require 'active_record/connection_adapters/abstract_mysql_adapter'
 
-
 module ActiveRecord
   module ConnectionAdapters
-    class AbstractMysqlAdapter < AbstractAdapter
+    class AbstractAdapter
       class Column < ConnectionAdapters::Column # :nodoc:
         def type_cast(val)
           self.type_cast_from_user(val)
