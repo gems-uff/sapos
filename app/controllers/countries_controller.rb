be486706f32f20e08b5b135d84aaf7211aa2d69d
Joao Felipe <joaofelipenp@gmail.com>
2015-07-28 22:38:48 -0300

    #188: Show nationality on country list

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -7,7 +7,7 @@ class CountriesController < ApplicationController
 
   active_scaffold :country do |config|
     config.list.sorting = {:name => 'ASC'}
-    config.list.columns = [:name]
+    config.list.columns = [:name, :nationality]
     config.create.label = :create_country_label
     config.create.columns = [:name, :nationality]
     config.update.label = :update_country_label
