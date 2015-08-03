5fcd366a8289fdf937d839f292d2294374983c8a
Joao Felipe <joaofelipenp@gmail.com>
2015-08-03 17:24:29 -0300

    #203: restore monkeypatch for sqlite

--- config/initializers/recordselect_patch.rb
+++ config/initializers/recordselect_patch.rb
@@ -2,6 +2,14 @@ require 'active_record/connection_adapters/abstract_mysql_adapter'
 
 module ActiveRecord
   module ConnectionAdapters
+    class AbstractMysqlAdapter < AbstractAdapter
+      class Column < ConnectionAdapters::Column # :nodoc:
+        def type_cast(val)
+          self.type_cast_from_user(val)
+        end
+      end
+    end
+
     class AbstractAdapter
       class Column < ConnectionAdapters::Column # :nodoc:
         def type_cast(val)
