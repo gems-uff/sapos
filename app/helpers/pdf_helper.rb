90954ddc45a114ff46530f464d37c45ee628b825
Joao Felipe <joaofelipenp@gmail.com>
2015-09-16 15:14:55 -0300

    #208: try to fix footer overflow

--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -7,7 +7,7 @@ require "prawn/measurement_extensions"
 
 module PdfHelper
   include ApplicationHelper
-  
+
   HEIGHT = 90
 
   def header(pdf, title, pdf_config, options={}, &block)
@@ -15,11 +15,11 @@ module PdfHelper
       type = options[:pdf_type] || :report; #report, transcript, grades_report, schedule
       pdf_type = :"use_at_#{type}"
       pdf_config = options[:pdf_config] || ReportConfiguration.where(pdf_type => true).order('`order` desc').first
-    end      
+    end
 
     pdf.bounding_box([0, pdf.cursor], :width => pdf.bounds.left + pdf.bounds.right, :height => HEIGHT) do
       pdf.bounding_box([0, pdf.cursor], :width => pdf.bounds.left + pdf.bounds.right - 170, :height => 68) do
-          
+
           pdf.stroke_bounds
           pdf.pad(15) do
             pdf.text "<b>#{pdf_config.text}</b>", :align => :center, font_size: 9, :inline_format => true
@@ -28,12 +28,12 @@ module PdfHelper
 
       pdf.bounding_box([0, pdf.cursor - 3], :width => pdf.bounds.left + pdf.bounds.right - 170, :height => 19) do
           pdf.stroke_bounds
-          
+
           pdf.fill_color '333399'
           pdf.fill_and_stroke_rectangle(
               [pdf.bounds.left, pdf.cursor],
               pdf.bounds.left + pdf.bounds.right,
-              19 
+              19
           )
           pdf.fill_color 'ffffff'
           pdf.pad(5) do
@@ -51,7 +51,7 @@ module PdfHelper
             yield
           else
             if not (pdf_config.image.nil? or pdf_config.image_identifier.nil?)
-              pdf.image(StringIO.new(pdf_config.image.read), 
+              pdf.image(StringIO.new(pdf_config.image.read),
                 :at => [pdf_config.x, HEIGHT - pdf_config.y],
                 :vposition => :top,
                 :scale => pdf_config.scale)
@@ -69,7 +69,7 @@ module PdfHelper
 
   def signature_footer(pdf, options={})
     x = 5
-    
+
     diff_width = options[:diff_width]
     diff_width ||= 0
 
@@ -123,7 +123,7 @@ module PdfHelper
 
   def datetime_footer(pdf, options={})
     x = 5
-    
+
     diff_width = options[:diff_width]
     diff_width ||= 0
 
@@ -149,7 +149,7 @@ module PdfHelper
       break if column.all? { |field| field.nil? }
       column_size = data_table.collect { |row| row[index + 1].nil? ? "" : row[index] }
       size = column_size.max_by { |field| field.to_s.size }.size
-      column.each_with_index do |field, i| 
+      column.each_with_index do |field, i|
         spaces = size >= field.to_s.size ? (" "*(size - field.to_s.size)) : ""
         rows[i] += field.to_s + spaces
       end
@@ -158,8 +158,7 @@ module PdfHelper
 
     rows.each do |row|
       pdf.move_down default_margin_indent
-    
-      pdf.text(row, :inline_format => true) 
+      pdf.text(row, :inline_format => true)
     end
   end
 
@@ -168,20 +167,20 @@ module PdfHelper
     type = options[:pdf_type] || :report; #report, transcript, grades_report, schedule
     pdf_type = :"use_at_#{type}"
     pdf_config = options[:pdf_config] || ReportConfiguration.where(pdf_type => true).order(order: :desc).first
-    
+
     prawn_document({
-      :page_size => "A4", 
-      :left_margin => 0.6.cm, 
-      :right_margin => (0.6.cm + ((options[:page_layout] == :landscape) ? 1.87 : 1.25)), 
-      :top_margin => 0.8.cm, 
-      :bottom_margin => (pdf_config.signature_footer ? 80 : 1.cm), 
+      :page_size => "A4",
+      :left_margin => 0.6.cm,
+      :right_margin => (0.6.cm + ((options[:page_layout] == :landscape) ? 1.87 : 1.25)),
+      :top_margin => 0.8.cm,
+      :bottom_margin => (pdf_config.signature_footer ? 96 : 1.cm),
       :filename => name
     }.merge(options)) do |pdf|
-      pdf.fill_color "000080" 
+      pdf.fill_color "000080"
       pdf.stroke_color '000080'
       header(pdf, title, pdf_config)
       yield pdf
-      
+
       pdf.repeat(:all, dynamic: true) do
         if pdf_config.signature_footer
           signature_footer(pdf)
@@ -200,7 +199,7 @@ module PdfHelper
           end
         end
         pdf.repeat(:all, dynamic: true) do
-          pdf.stamp_at "watermark", [80, 0] 
+          pdf.stamp_at "watermark", [80, 0]
         end
       end
     end
