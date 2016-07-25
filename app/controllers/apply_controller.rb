a654c4f455014ab8e441c00a7c8027c3dc9306c5
Bruno Santos <brunodess@gmail.com>
2016-07-24 23:56:15 -0300

    Fixed collection_checkbox insert

--- app/controllers/apply_controller.rb
+++ app/controllers/apply_controller.rb
@@ -1,7 +1,7 @@
 class ApplyController < ApplicationController
   skip_authorization_check
   skip_before_filter :authenticate_user!
-  before_action :set_application_process, only: [:new, :student_getid, :student_find, :fill_form, :student_confirm, :create, :update]
+  before_action :set_application_process, only: [:student_getid, :student_find, :fill_form, :student_confirm, :create, :update]
   before_action :check_token, only: [:create_student_application]
 
   def index
@@ -100,7 +100,7 @@ class ApplyController < ApplicationController
 
   def create_student_application
     @student_application = StudentApplication.new(student_application_params)
-
+    #raise 'debug'
     if @student_application.save
       @student_application.letter_requests.each do |lr|
         ApplyMailer.letter_request_mail(lr).deliver_later
@@ -176,7 +176,7 @@ class ApplyController < ApplicationController
 
   def student_application_params
     params.require(:student_application).permit(:student_id, :application_process_id,
-                                                 form_field_inputs_attributes:[:id, :form_field_id, :input, :_destroy],
+                                                 form_field_inputs_attributes:[:id, :form_field_id, input: []],
                                                 form_text_inputs_attributes:[:id, :form_field_id, :input, :_destroy],
                                                 form_file_uploads_attributes:[:id, :student_application_id, :form_field_id, :file, :_destroy],
                                                 letter_requests_attributes:[:id, :professor_email, :student_application_id, :access_token, :is_filled, :_destroy]
@@ -191,7 +191,7 @@ class ApplyController < ApplicationController
 
                                     student_applications_attributes:[:id, :student_id, :application_process_id, :_destroy],
 
-                                    form_field_inputs_attributes:[:id, :form_field_id, :input, :_destroy],
+                                    form_field_inputs_attributes:[:id, :form_field_id, input: []],
                                     form_text_inputs_attributes:[:id, :form_field_id, :input, :_destroy],
                                     form_file_uploads_attributes:[:id, :student_application_id, :form_field_id, :file, :_destroy],
                                     letter_requests_attributes:[:id, :professor_email, :student_application_id, :access_token, :is_filled, :_destroy]
