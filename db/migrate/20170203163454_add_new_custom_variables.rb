b08284a37029f816585d8bb4213759fc1fcde55a
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2017-02-03 17:37:54 -0200

    fixing error in migration file add_new_custom_variables. changing :name by :description

--- db/migrate/20170203163454_add_new_custom_variables.rb
+++ db/migrate/20170203163454_add_new_custom_variables.rb
@@ -1,8 +1,8 @@
 class AddNewCustomVariables < ActiveRecord::Migration
   def change
 
-    CustomVariable.where(:variable => "minimum_grade_for_approval").first or CustomVariable.create(:name=>"Nota mínima para aprovação", :variable =>"minimum_grade_for_approval", :value => "6.0")
-    CustomVariable.where(:variable => "grade_of_disapproval_for_absence").first or CustomVariable.create(:name=>"Nota de reprovação por falta", :variable =>"grade_of_disapproval_for_absence", :value => "1.0")
+    CustomVariable.where(:variable => "minimum_grade_for_approval").first or CustomVariable.create(:description=>"Nota mínima para aprovação", :variable =>"minimum_grade_for_approval", :value => "6.0")
+    CustomVariable.where(:variable => "grade_of_disapproval_for_absence").first or CustomVariable.create(:description=>"Nota de reprovação por falta", :variable =>"grade_of_disapproval_for_absence", :value => "1.0")
 
   end
 end
