e991ed869d1bf671dc09cfa5b1e23e0d618ca2fe
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2017-02-06 14:30:40 -0200

    adding new custom variables to file seed.rb and fixing default value of grade_of_disapproval_for_absence in new migration file

--- db/migrate/20170203163454_add_new_custom_variables.rb
+++ db/migrate/20170203163454_add_new_custom_variables.rb
@@ -2,7 +2,7 @@ class AddNewCustomVariables < ActiveRecord::Migration
   def change
 
     CustomVariable.where(:variable => "minimum_grade_for_approval").first or CustomVariable.create(:description=>"Nota mínima para aprovação", :variable =>"minimum_grade_for_approval", :value => "6.0")
-    CustomVariable.where(:variable => "grade_of_disapproval_for_absence").first or CustomVariable.create(:description=>"Nota de reprovação por falta", :variable =>"grade_of_disapproval_for_absence", :value => "1.0")
+    CustomVariable.where(:variable => "grade_of_disapproval_for_absence").first or CustomVariable.create(:description=>"Nota de reprovação por falta", :variable =>"grade_of_disapproval_for_absence", :value => "0.0")
 
   end
 end
