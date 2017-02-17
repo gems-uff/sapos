3e25fc32022a94c7c7425d290a0ffaf9de56f654
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2017-02-17 17:12:36 -0200

    issue #215 first version and adding constraints between custom variables minimum_grade_for_approval and grade_of_disapproval_for_absence

--- app/models/custom_variable.rb
+++ app/models/custom_variable.rb
@@ -17,7 +17,9 @@ class CustomVariable < ActiveRecord::Base
   }
 
   validates :variable, :presence => true
-
+  validate :check_constraints_between_variables
+  before_destroy :validate_destroy
+  before_validation :validate_create, :on => :create
 
   def self.single_advisor_points
   	config = CustomVariable.find_by_variable(:single_advisor_points)
@@ -56,16 +58,52 @@ class CustomVariable < ActiveRecord::Base
 
   def self.minimum_grade_for_approval
     config = CustomVariable.find_by_variable(:minimum_grade_for_approval)
-    config.nil? ? 60 : ((config.value.tr(',', '.').to_f)*10.0).to_i 
+    if config.nil? or config.value.blank?
+      return 60
+    else
+      return ((config.value.tr(',','.').to_f)*10.0).to_i
+    end	    
   end
 
   def self.grade_of_disapproval_for_absence
     config = CustomVariable.find_by_variable(:grade_of_disapproval_for_absence)
-    if config.nil? or config.value.nil? or (config.value.gsub(/\s+/, "").length == 0)
+    if config.nil? or config.value.blank?
       return nil
     else      
       return ((config.value.tr(',', '.').to_f)*10.0).to_i
     end
   end
+
+  def to_label
+    "#{self.variable}"
+  end	  
+
+  private
+
+  def check_constraints_between_variables
+    case self.variable
+    when "minimum_grade_for_approval"
+      self.errors.add(:value, I18n.translate("activerecord.errors.models.custom_variable.minimum_grade_for_approval_not_gt_grade_of_disapproval_for_absence", :minimum_grade_for_approval=>(self.value.blank? ? 6.0 : self.value.tr(',','.')).to_s, :grade_of_disapproval_for_absence=>(CustomVariable.grade_of_disapproval_for_absence.to_f/10.0).to_s)) if (not CustomVariable.grade_of_disapproval_for_absence.nil? ) and (   ( (self.value.blank?) and ( CustomVariable.grade_of_disapproval_for_absence >= 60 ) )   or   ( (not self.value.blank?) and ( CustomVariable.grade_of_disapproval_for_absence >= ((self.value.tr(',','.').to_f)*10.0).to_i  )    ) )
+    when "grade_of_disapproval_for_absence"
+      self.errors.add(:value, I18n.translate("activerecord.errors.models.custom_variable.grade_of_disapproval_for_absence_not_lt_minimum_grade_for_approval", :grade_of_disapproval_for_absence=>self.value.tr(',','.'),:minimum_grade_for_approval=>(CustomVariable.minimum_grade_for_approval.to_f/10.0).to_s)) if (not self.value.blank?) and ( ((self.value.tr(',','.').to_f)*10.0).to_i >= CustomVariable.minimum_grade_for_approval )
+    end
+  end
+
+  def validate_destroy
+    case self.variable
+    when "minimum_grade_for_approval"
+      self.errors.add(:base, I18n.translate("activerecord.errors.models.custom_variable.validate_destroy_of_minimum_grade_for_approval")) if (not CustomVariable.grade_of_disapproval_for_absence.nil?) and ( CustomVariable.grade_of_disapproval_for_absence >= 60 )      
+    end	    
+    self.errors.blank?
+  end
+
+  def validate_create
+    case self.variable
+    when "minimum_grade_for_approval", "grade_of_disapproval_for_absence"
+      if not CustomVariable.find_by_variable(self.variable).nil?
+        self.errors.add(:base, I18n.translate("activerecord.errors.models.custom_variable.check_duplicate", :variable=>self.variable))	
+      end      
+    end
+  end	  
 
 end
