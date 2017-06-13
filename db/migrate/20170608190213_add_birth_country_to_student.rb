f7b3851cc84a52f8cad400e165ed2423e5fb6af3
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2017-06-13 18:35:35 -0300

    #188 and #189 changed migration to add birth_country to student

--- db/migrate/20170608190213_add_birth_country_to_student.rb
+++ db/migrate/20170608190213_add_birth_country_to_student.rb
@@ -1,5 +1,6 @@
 class AddBirthCountryToStudent < ActiveRecord::Migration
   def change
-    add_reference :students, :birth_country, index: true, foreign_key: true
+    add_column :students, :birth_country_id, :integer
+    add_index :students, :birth_country_id
   end
 end
