077227d6a6e2d4267021dfeb8031bdc8144abf78
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2017-06-20 17:31:39 -0300

    #188 and #189 add migration to set birth_country in student where birth_city or birth_state are defined

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608190213) do
+ActiveRecord::Schema.define(version: 20170619203122) do
 
   create_table "accomplishments", force: :cascade do |t|
     t.integer  "enrollment_id"
