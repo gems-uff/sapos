b8f434a379b197ad0061172ed2f32bc3d119280b
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2017-07-27 14:09:10 -0300

    #227 fix 20170719151616_add_suporte_role.rb migration

--- db/migrate/20170719151616_add_suporte_role.rb
+++ db/migrate/20170719151616_add_suporte_role.rb
@@ -1,5 +1,5 @@
 class AddSuporteRole < ActiveRecord::Migration
     def self.up
-        Role.create{:id => 7, :name => "Suporte", :description => "Suporte (inserir fotos de alunos)"}
+        Role.create(:id => 7, :name => "Suporte", :description => "Suporte (inserir fotos de alunos)")
     end
 end
