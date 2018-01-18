89cdc625033e562f307d9bbf947e9a2fae40b01c
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2018-01-18 16:52:38 -0200

    #236 changing the config to load files from the lib directory

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Sapos
     # Allow the notifier to send emails
     config.should_send_emails = false
 
-
+    config.eager_load_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
