befda6535c937a65478fcc7e6885b8299db22041
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2018-02-22 16:14:08 -0300

    #244 adding permitted parameters to course recordselect

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -2,4 +2,7 @@
 # This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
 
 module CoursesHelper
+  def permit_rs_browse_params
+    [:available, :page, :update, :utf8]
+  end
 end
