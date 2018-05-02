21951dfb2dea7ddaae242098b984815ecbf6a475
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2018-05-02 12:27:21 -0300

    #261 removed deprecated method 'scoped' (from rails 3) to mount a query. Zeros were added to the left of the day and month to keep the 'YYYY-MM-DD' format in the search query

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -125,7 +125,7 @@ class EnrollmentsController < ApplicationController
 
   def self.condition_for_accomplishments_column(column, value, like_pattern)
     return "" if value[:phase].blank?
-    date = value.nil? ? value : "#{value[:year]}-#{value[:month]}-#{value[:day]}"
+    date = value.nil? ? value : "#{value[:year]}-#{(value[:month].size == 1)? '0' + value[:month] : value[:month]}-#{(value[:day].size == 1)? '0' + value[:day] : value[:day]}"
     phase = value[:phase] == "all" ? nil : value[:phase]
     if (value[:phase] == "all")
       enrollments_ids = Enrollment.with_all_phases_accomplished_on(date)
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -106,7 +106,7 @@ class Enrollment < ApplicationRecord
     enrollments_with_all_phases_accomplished = []
     enrollments.each do |enrollment|
       accomplished_phases = Accomplishment.where("enrollment_id = :enrollment_id and DATE(conclusion_date) <= DATE(:conclusion_date)", :enrollment_id => enrollment.id, :conclusion_date => date).map { |ac| ac.phase }
-      phases_duration = PhaseDuration.where("level_id = :level_id", :level_id => enrollment.level_id).scoped
+      phases_duration = PhaseDuration.where("level_id = :level_id", :level_id => enrollment.level_id)
       phases_duration = phases_duration.where("phase_id not in (:accomplished_phases)", :accomplished_phases => accomplished_phases) unless accomplished_phases.blank?
       enrollments_with_all_phases_accomplished << enrollment.id if phases_duration.blank?
     end
