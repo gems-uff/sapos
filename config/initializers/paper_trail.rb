18fd43e21cfddac2edf4d29536c3233048ccab55
Carlos Eduardo Cabral da Cunha <eduardosolrac286@gmail.com>
2020-05-27 19:09:50 -0300

    #325 removing track_associations line from paper_trail gem initializer file

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,4 +1,3 @@
 # Copyright (c) Universidade Federal Fluminense (UFF).
 # This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
 
-PaperTrail.config.track_associations = false
