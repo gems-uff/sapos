3ae02053a4f3ecc5badbb6f5540dcc5851ce1ac1
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2021-08-09 11:54:30 -0300

    #346: use redirect_email variable to prevent or redirect devise emails

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -7,6 +7,21 @@ class DeviseMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
 
+  def headers_for(action, opts)
+    headers = super
+    unless CustomVariable.redirect_email.nil?
+      headers[:subject] = headers[:subject] + " (Originalmente para #{headers[:to]})"
+      headers[:to] = CustomVariable.redirect_email
+    end
+    headers
+  end
+
+  def devise_mail(record, action, opts = {}, &block)
+    if CustomVariable.redirect_email != ""
+      super
+    end
+  end
+
   def invitation_instructions(record, token, opts={})
     @token = token
     devise_mail(record, :invitation_instructions, opts) do |format|
