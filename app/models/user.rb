64c18c432d9179ae954483b87efc87201317a520
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2021-08-18 11:18:52 -0300

    #346: do not send confirmation email for invitations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   devise :invitable, :database_authenticatable, :recoverable, :rememberable, :registerable, :trackable, :confirmable,
          :lockable
 
+  after_create :skip_confirmation_notification!, unless: Proc.new { self.invitation_token.nil? }
+
   validates :email, :presence => true, :uniqueness => true
   validates :name, :presence => true
   validate :role_valid?
