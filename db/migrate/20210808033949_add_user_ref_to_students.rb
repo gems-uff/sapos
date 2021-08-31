390862fd022d06a05339f99d6fcb60be0a7c32fd
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2021-08-31 13:06:12 -0300

    #361: fix previous migration for old database instances

--- db/migrate/20210808033949_add_user_ref_to_students.rb
+++ db/migrate/20210808033949_add_user_ref_to_students.rb
@@ -4,12 +4,11 @@ class AddUserRefToStudents < ActiveRecord::Migration[6.0]
     remove_index :users, name: "index_users_on_invited_by_id" if index_exists?(:users, name: "index_users_on_invited_by_id")
     remove_index :users, name: "index_users_on_invited_by_type_and_invited_by_id" if index_exists?(:users, name: "index_users_on_invited_by_type_and_invited_by_id")
 
-    change_column :users, :id, :bigint
-    change_column :professors, :user_id, :bigint
+    change_column :users, :id, :integer, limit: 8, unique: true, null: false, auto_increment: true
+    change_column :professors, :user_id, :integer, limit: 8, unique: true, null: false, auto_increment: true
     add_index :professors, :user_id, name: "index_professors_on_user_id" unless index_exists?(:professors, :user_id)
-    change_column :users, :invited_by_id, :bigint
+    change_column :users, :invited_by_id, :integer, limit: 8, unique: true, null: false, auto_increment: true
     add_index :users, :invited_by_id, name: "index_users_on_invited_by_id" unless index_exists?(:users, :invited_by_id)
-    change_column :users, :invited_by_id, :bigint
     add_index :users, [:invited_by_type, :invited_by_id], name: "index_users_on_invited_by_type_and_invited_by_id" unless index_exists?(:users, [:invited_by_type, :invited_by_id])
 
     add_reference :students, :user, foreign_key: { on_delete: :nullify }
