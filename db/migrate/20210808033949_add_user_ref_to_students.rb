9e0c455f3bd4e19d6ec89158be0ec9c9bd062a78
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2021-08-31 21:16:51 -0300

    #361: fix old migration

--- db/migrate/20210808033949_add_user_ref_to_students.rb
+++ db/migrate/20210808033949_add_user_ref_to_students.rb
@@ -5,9 +5,9 @@ class AddUserRefToStudents < ActiveRecord::Migration[6.0]
     remove_index :users, name: "index_users_on_invited_by_type_and_invited_by_id" if index_exists?(:users, name: "index_users_on_invited_by_type_and_invited_by_id")
 
     change_column :users, :id, :integer, limit: 8, unique: true, null: false, auto_increment: true
-    change_column :professors, :user_id, :integer, limit: 8, unique: true, null: false, auto_increment: true
+    change_column :professors, :user_id, :integer, limit: 8
     add_index :professors, :user_id, name: "index_professors_on_user_id" unless index_exists?(:professors, :user_id)
-    change_column :users, :invited_by_id, :integer, limit: 8, unique: true, null: false, auto_increment: true
+    change_column :users, :invited_by_id, :integer, limit: 8
     add_index :users, :invited_by_id, name: "index_users_on_invited_by_id" unless index_exists?(:users, :invited_by_id)
     add_index :users, [:invited_by_type, :invited_by_id], name: "index_users_on_invited_by_type_and_invited_by_id" unless index_exists?(:users, [:invited_by_type, :invited_by_id])
 
