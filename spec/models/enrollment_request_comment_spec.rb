714ab032db9fd2930998b0da7dd93425f07773e5
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2021-09-03 20:18:03 -0300

    #347: tests for enrollment request comments

--- spec/models/enrollment_request_comment_spec.rb
+++ spec/models/enrollment_request_comment_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
 
 RSpec.describe EnrollmentRequestComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:enrollment_request_comment) { EnrollmentRequestComment.new }
+  subject { enrollment_request_comment }
+  describe "Validations" do
+    describe "message" do
+      context "should be valid when" do
+        it "message is not null" do
+          enrollment_request_comment.message = "abc"
+          expect(enrollment_request_comment).to have(0).errors_on :message
+        end
+      end
+      context "should have error blank when" do
+        it "message is null" do
+          enrollment_request_comment.message = nil
+          expect(enrollment_request_comment).to have_error(:blank).on :message
+        end
+      end
+    end
+    describe "enrollment_request" do
+      context "should be valid when" do
+        it "enrollment_request is not null" do
+          enrollment_request_comment.enrollment_request = EnrollmentRequest.new
+          expect(enrollment_request_comment).to have(0).errors_on :enrollment_request
+        end
+      end
+      context "should have error blank when" do
+        it "enrollment_request is null" do
+          enrollment_request_comment.enrollment_request = nil
+          expect(enrollment_request_comment).to have_error(:blank).on :enrollment_request
+        end
+      end
+    end
+    describe "user" do
+      context "should be valid when" do
+        it "user is not null" do
+          enrollment_request_comment.user = User.new
+          expect(enrollment_request_comment).to have(0).errors_on :user
+        end
+      end
+      context "should have error blank when" do
+        it "user is null" do
+          enrollment_request_comment.user = nil
+          expect(enrollment_request_comment).to have_error(:blank).on :user
+        end
+      end
+    end
+  end
 end
