6ac34cb89a6a2dee09fd5b7fe63836354ef53207
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2021-09-04 18:04:30 -0300

    #347: use end of day as class schedule enrollment end time

--- app/helpers/class_schedules_helper.rb
+++ app/helpers/class_schedules_helper.rb
@@ -1,2 +1,21 @@
+# Copyright (c) Universidade Federal Fluminense (UFF).
+# This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
+
 module ClassSchedulesHelper
+
+  def enrollment_start_form_column(record, options)
+    unless record.persisted?
+      record.enrollment_start = DateTime.current.midnight
+    end
+    config = ActiveScaffold::Config::Core.new(:class_schedule)
+    active_scaffold_input_date_picker config.columns[:enrollment_start], options
+  end
+
+  def enrollment_end_form_column(record, options)
+    unless record.persisted?
+      record.enrollment_end = DateTime.current.midnight + 1.day - 1.second
+    end
+    config = ActiveScaffold::Config::Core.new(:class_schedule)
+    active_scaffold_input_date_picker config.columns[:enrollment_end], options
+  end
 end
