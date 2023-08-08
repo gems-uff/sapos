b61d8c3fb0ea7ca44e9aca89175b6847f9f912a5
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2023-08-08 14:18:44 -0300

    Fix exception on version search

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,58 +1,59 @@
-# encoding: utf-8
 # Copyright (c) Universidade Federal Fluminense (UFF).
 # This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
 
+# frozen_string_literal: true
+
 module VersionsHelper
-	def item_type_column(record, column)
-		I18n.t("activerecord.models.#{record.item_type.underscore}.one")
-	end
+  def item_type_column(record, column)
+    I18n.t("activerecord.models.#{record.item_type.underscore}.one")
+  end
 
-	def current_object_column(record, column)
-		object_id = record.item_id.to_i
-		model_str = record.item_type
-		model = model_str.constantize
-		if model.find_by_id(object_id).nil?
-			I18n.t("activerecord.attributes.version.current_object_destroyed")
-		else
+  def current_object_column(record, column)
+    object_id = record.item_id.to_i
+    model_str = record.item_type
+    model = model_str.constantize
+    if model.find_by_id(object_id).nil?
+      I18n.t("activerecord.attributes.version.current_object_destroyed")
+    else
 
-			begin
-				path = send((model_str.underscore + "_path").to_sym, object_id)
-				#route = Rails.application.routes.recognize_path(path)
-				link_to(h(model.find(object_id).to_label), path) if object_id != 0
-			rescue NoMethodError
-				I18n.t("activerecord.attributes.version.relationship_object") if object_id != 0
-			end
-		end
-	end
+      begin
+        path = send((model_str.underscore + "_path").to_sym, object_id)
+        # route = Rails.application.routes.recognize_path(path)
+        link_to(h(model.find(object_id).to_label), path) if object_id != 0
+      rescue NoMethodError
+        I18n.t("activerecord.attributes.version.relationship_object") if object_id != 0
+      end
+    end
+  end
 
-	def event_column(record, column)
-		I18n.t("activerecord.attributes.version.event_options.#{record.event}")
-	end
+  def event_column(record, column)
+    I18n.t("activerecord.attributes.version.event_options.#{record.event}")
+  end
 
-	def user_column(record, column)
-		user_id = record.whodunnit.to_i
-		link_to(h(User.find(user_id).name), user_path(User.find(user_id))) if user_id != 0
-	end
+  def user_column(record, column)
+    user_id = record.whodunnit.to_i
+    link_to(h(User.find(user_id).name), user_path(User.find(user_id))) if user_id != 0
+  end
 
-	def created_at_column(record, column)
-		return record.created_at.strftime('%d/%m/%Y às %H:%M')
-	end
+  def created_at_column(record, column)
+    record.created_at.strftime("%d/%m/%Y às %H:%M")
+  end
 
-	def old_version_show_column(record, column)
-		if not record.object.nil?
-			property_list = ''
-			raw_property_list = record.object.split("\n")
+  def old_version_show_column(record, column)
+    if record.object.present?
+      property_list = ""
+      raw_property_list = record.object.split("\n")
 
-			for property in raw_property_list
-				if not property.end_with? ': ' and 
-					not property.include? 'password' and 
-					not property.include? '_at' and
-					not property.include? 'sign_in'
-					property_list += (property + "<br>") if property != '---' #a string do objeto inicia com um ---
-				end
-			end
-
-			html_escape(property_list).gsub("&lt;br&gt;", "<br>").html_safe
-		end
-	end
+      for property in raw_property_list
+        if !property.end_with?(": ") &&
+          !property.include?("password") &&
+          !property.include?("_at") &&
+          !property.include?("sign_in")
+          # a string do objeto inicia com um ---
+          property_list += (property + "<br>") if property != "---"
+        end
+      end
+      html_escape(property_list).gsub("&lt;br&gt;", "<br>").html_safe
+    end
+  end
 end
---
+++ spec/features/versions_spec.rb
@@ -0,0 +1,72 @@
+# Copyright (c) Universidade Federal Fluminense (UFF).
+# This file is part of SAPOS. Please, consult the license terms in the LICENSE file.
+
+# frozen_string_literal: true
+
+# ToDo: fix search to look for content
+
+require "spec_helper"
+
+RSpec.describe "Versions features", type: :feature do
+  let(:url_path) { "/versions" }
+  let(:plural_name) { "versions" }
+  let(:model) { Version }
+  before(:all) do
+    @destroy_later = []
+    @destroy_all = []
+    @destroy_all << @role_adm = FactoryBot.create(:role_administrador)
+    @destroy_all << @user = create_confirmed_user(@role_adm)
+    @destroy_all << @country1 = FactoryBot.create(:country, name: "Brasil", nationality: "brasileiro(a)")
+  end
+  after(:each) do
+    @destroy_later.each(&:delete)
+    @destroy_later.clear
+  end
+  after(:all) do
+    @destroy_all.each(&:delete)
+    @destroy_all.clear
+  end
+
+  describe "view list page", js: true  do
+    before(:each) do
+      login_as(@user)
+      visit url_path
+    end
+
+    it "should show table" do
+      expect(page).to have_content "Log"
+      expect(page.all("tr th").map(&:text)).to eq [
+        "Modelo", "Versão Atual", "Ação", "Usuário", "Modificado em", ""
+      ]
+    end
+
+    it "should sort the list by created_at, desc" do
+      # First item is the user login
+      expect(page.all("tr:nth-child(1) td.item_type-column").map(&:text)).to eq ["Usuário"]
+      # Second item is the country
+      expect(page.all("tr:nth-child(2) td.item_type-column").map(&:text)).to eq ["País"]
+    end
+
+    it "should not have insert link" do
+      expect(page).not_to have_content "Adicionar"
+    end
+
+    it "should not have edit link" do
+      expect(page).not_to have_selector("#as_#{plural_name}-edit-#{model.last.id}-link")
+    end
+  end
+
+  describe "search page", js: true do
+    before(:each) do
+      login_as(@user)
+      visit url_path
+      click_link "Buscar"
+    end
+
+    it "should be able to search by model name" do
+      fill_in "search", with: "Country"
+      sleep(0.8)
+      expect(page.all("tr td.item_type-column").map(&:text)).to eq ["País"]
+    end
+  end
+end
