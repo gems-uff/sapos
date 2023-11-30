f0e6d49c179334f621b495093da6541eeb63a6e8
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2023-11-29 23:12:57 -0300

    Fix binary filtering

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -6,5 +6,32 @@
 # sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
 # notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :binary
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
 ]
+
+# Override of render_bind to force a truncation of the binary column of carrier_wave_files
+# https://github.com/rails/rails/blob/master/activerecord/lib/active_record/log_subscriber.rb#L86
+module LogTruncater
+  def render_bind(attr, value)
+    case attr
+    when ActiveModel::Attribute
+      if attr.type.binary? && attr.value
+        value = "<#{attr.value_for_database.to_s.bytesize} bytes of binary data>"
+      # new code
+      elsif attr.name.to_s == "binary" && attr.value
+        value = "<#{attr.value_for_database.to_s.bytesize} bytes of binary data>"
+      # << end of new code
+      end
+    when Array
+      attr = attr.first
+    else
+      attr = nil
+    end
+
+    [attr&.name, value]
+  end
+end
+
+class ActiveRecord::LogSubscriber
+  prepend LogTruncater
+end
