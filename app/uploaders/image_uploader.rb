d1056d33a0d6c094c7eddd084d5d590ba75196c4
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2024-01-09 19:47:38 -0300

    remove uploader reference counter: do not remove old uploads from DB to allow manual recovery

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  include UploaderReferenceCounter
 
   # Choose what kind of storage to use for this uploader:
   storage :active_record
