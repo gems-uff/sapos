3607c984e1c306f9899c0e1ed40e2bcfccb60e96
IMonardez <igormonardez@id.uff.br>
2024-06-06 18:17:42 -0300

    ISSUE #448 - adjustments at schema

--- db/schema.rb
+++ db/schema.rb
@@ -256,6 +256,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_27_174758) do
     t.index ["professor_id"], name: "index_advisements_on_professor_id"
   end
 
+  create_table "affiliations", force: :cascade do |t|
+    t.integer "professor_id"
+    t.integer "institution_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["institution_id"], name: "index_affiliations_on_institution_id"
+    t.index ["professor_id"], name: "index_affiliations_on_professor_id"
+  end
+
   create_table "allocations", force: :cascade do |t|
     t.string "day", limit: 255
     t.string "room", limit: 255
@@ -619,6 +630,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_27_174758) do
     t.string "subject", limit: 255
     t.text "body"
     t.string "attachments_file_names"
+    t.string "reply_to", limit: 255
     t.index ["notification_id"], name: "index_notification_logs_on_notification_id"
   end
 
@@ -701,7 +713,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_27_174758) do
     t.string "siape", limit: 255
     t.string "enrollment_number", limit: 255
     t.string "identity_issuing_place", limit: 255
-    t.integer "institution_id"
     t.string "email", limit: 255
     t.date "academic_title_date"
     t.integer "academic_title_country_id"
@@ -715,8 +726,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_27_174758) do
     t.index ["city_id"], name: "index_professors_on_city_id"
     t.index ["cpf"], name: "index_professors_on_cpf"
     t.index ["email"], name: "index_professors_on_email"
-    t.index ["institution_id"], name: "index_professors_on_institution_id"
     t.index ["user_id"], name: "index_professors_on_user_id"
+  end
+
+  create_table "program_levels", force: :cascade do |t|
+    t.integer "level", null: false
+    t.date "start_date", null: false
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "queries", force: :cascade do |t|
