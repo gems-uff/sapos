e55a7678298ef028f66245b7a1e02e072096f444
¨caiovelp¨ <cparanhos@id.uff.br>
2024-08-27 19:42:04 -0300

    Modify query and assertion models

--- app/models/assertion.rb
+++ app/models/assertion.rb
@@ -3,6 +3,18 @@
 class Assertion < ApplicationRecord
   has_paper_trail
 
+  belongs_to :query, inverse_of: :assertions, optional: false
+
+  DERIVATION_DEFS = {
+    "data_consulta" => { name: "data_consulta", value_type: "Date" }
+  }
+  DERIVED_PARAMS = {
+    "numero_ultimo_semestre" => "data_consulta",
+    "ano_ultimo_semestre" => "data_consulta",
+    "numero_semestre_atual" => "data_consulta",
+    "ano_semestre_atual" => "data_consulta",
+  }
+
   validates :name, presence: true
 
   def to_label
