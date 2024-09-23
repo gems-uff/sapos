7a9acf2e17560162c00f875877037cbd52af5a2e
¨caiovelp¨ <cparanhos@id.uff.br>
2024-09-23 17:40:51 -0300

    Create a new page if explode the default box

--- app/helpers/assertions_pdf_helper.rb
+++ app/helpers/assertions_pdf_helper.rb
@@ -32,8 +32,18 @@ module AssertionsPdfHelper
   def assertion_box_text_print(pdf, template, values, records = [], box_width, box_height)
     pdf.move_down 30
     text = replace_placeholders(template, values, records)
-    pdf.text_box text, at: [(pdf.bounds.width - box_width) / 2, pdf.cursor], width: box_width, height: box_height, align: :justify, inline_format: true
-  end
+
+    lines = pdf.text_box text, at: [(pdf.bounds.width - box_width) / 2, pdf.cursor], width: box_width, height: box_height, align: :justify, inline_format: true, dry_run: true
+
+    while lines.size > 0
+      not_printed_text_length = lines.map { |line| line[:text].length }.sum
+      text = text[-not_printed_text_length..-1]
+
+      pdf.start_new_page
+      lines = pdf.text_box text, at: [(pdf.bounds.width - box_width) / 2, pdf.cursor], width: box_width, height: box_height, align: :justify, inline_format: true, dry_run: true
+    end
+
+    end
 
   def assertion_table(pdf, options = {})
     assertion = options[:assertion]
