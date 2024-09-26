4651d09c44e4d7513b1cfbfc6a07ff0a01d8a534
¨caiovelp¨ <cparanhos@id.uff.br>
2024-09-26 18:31:13 -0300

    Remove unnecessary migration

--- db/migrate/20242204223146_add_box_dimensions_to_assertions.rb
+++
@@ -1,6 +0,0 @@
-class AddBoxDimensionsToAssertions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :assertions, :assertion_box_width, :integer, default: 400
-    add_column :assertions, :assertion_box_height, :integer, default: 580
-  end
-end
