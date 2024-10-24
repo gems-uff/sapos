d2ed6fef680dcc2ff4e4f463b3cad5f595697485
IMonardez <igormonardez@id.uff.br>
2024-10-24 14:52:44 -0300

    ISSUE #448: Fixing the on_date scope logic

--- app/models/program_level.rb
+++ app/models/program_level.rb
@@ -8,6 +8,6 @@ class ProgramLevel < ApplicationRecord
   validates :end_date, presence: false
 
   scope :active, -> { where(end_date: nil) }
-  scope :on_date, -> (date) { where("program_levels.start_date <= ? AND program_levels.end_date > ? OR program_levels.end_date is null", date, date)}
+  scope :on_date, -> (date) { where("program_levels.start_date <= ? (AND program_levels.end_date > ? OR program_levels.end_date is null)", date, date)}
 
 end
