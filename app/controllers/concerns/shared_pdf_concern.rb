f18adec90c8be0a259c27376250fa00bf556688f
Anderson Meireles <anderson_meireles@id.uff.br>
2024-11-20 17:33:32 -0300

    add back default file name for reports

--- app/controllers/concerns/shared_pdf_concern.rb
+++ app/controllers/concerns/shared_pdf_concern.rb
@@ -34,7 +34,7 @@ module SharedPdfConcern
     )
   end
 
-  def render_enrollments_academic_transcript_pdf(enrollment, filename)
+  def render_enrollments_academic_transcript_pdf(enrollment, filename = "transcript.pdf")
     class_enrollments = enrollment.class_enrollments
       .where(situation: ClassEnrollment::APPROVED)
       .joins(:course_class)
@@ -55,7 +55,7 @@ module SharedPdfConcern
     )
   end
 
-  def render_enrollments_grades_report_pdf(enrollment, filename)
+  def render_enrollments_grades_report_pdf(enrollment, filename = "grades_report.pdf")
     class_enrollments = enrollment.class_enrollments
       .where(situation: ClassEnrollment::APPROVED)
       .joins(:course_class)
