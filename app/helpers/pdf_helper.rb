096cbf5a0f19fd3a85b25cf2adb1bdcd5b340bbb
Anderson Meireles <anderson_meireles@id.uff.br>
2025-01-07 22:22:11 -0300

    removing watermark and changing ability to let professors generate reports only with qr code

--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -341,22 +341,6 @@ module PdfHelper
           datetime_footer(pdf)
         end
       end
-      if options[:watermark]
-        pdf.create_stamp("watermark") do
-          pdf.rotate(60, origin: [0, 0]) do
-            pdf.fill_color "993333"
-            pdf.font("FreeMono", size: 22) do
-              pdf.draw_text(
-                I18n.t("pdf_content.professor_watermark"), at: [0, 0]
-              )
-            end
-            pdf.fill_color "000000"
-          end
-        end
-        pdf.repeat(:all, dynamic: true) do
-          pdf.stamp_at "watermark", [80, 0]
-        end
-      end
     end
 
     if pdf_config.qr_code_signature && !pdf_config.preview
