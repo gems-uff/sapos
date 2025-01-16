eb3204b05ffe7bbc399619f2881065b2ee3bd555
Anderson Meireles <anderson_meireles@id.uff.br>
2025-01-16 18:43:39 -0300

    fix report factory

--- spec/factories/factory_report.rb
+++ spec/factories/factory_report.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :report do
     association :user, factory: :user
+    file_name { "report_#{SecureRandom.hex(4)}.pdf" }
     created_at { Time.now }
     expires_at { 1.year.from_now }
   end
