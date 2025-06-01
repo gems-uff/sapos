26faced61ab8428f0917d778118727abcc17f435
SAASVUUV <lipeseabra@gmail.com>
2025-05-31 22:22:58 -0300

    Fix the down of RemoveRoleIdFromUsers

--- db/migrate/20250426174500_remove_role_id_from_user.rb
+++ db/migrate/20250426174500_remove_role_id_from_user.rb
@@ -1,5 +1,13 @@
 class RemoveRoleIdFromUser < ActiveRecord::Migration[7.0]
-  def change
+  def up
     remove_column :users, :role_id, :integer
+  end
+
+  def down
+    add_column :users, :role_id, :integer
+
+    User.find_each do |user|
+      user.update!(role_id: user.user_max_role)
+    end
   end
 end
