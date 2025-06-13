9db0961cf8a3b429bf0bc382780c7f3b30a3cbcf
SAASVUUV <lipeseabra@gmail.com>
2025-06-13 10:14:31 -0300

    ISSUE #547: fix the last migration

--- db/migrate/20250611002344_remove_has_grades_report_pdf_attachment_from_notifications.rb
+++ db/migrate/20250611002344_remove_has_grades_report_pdf_attachment_from_notifications.rb
@@ -1,5 +1,9 @@
 class RemoveHasGradesReportPdfAttachmentFromNotifications < ActiveRecord::Migration[7.0]
   def up
+    grade_report_message = I18n.t("active_scaffold.notification.grades_report_path")
+    Notification.find_each do |noti|
+      noti.update!(body_template: noti.body_template + grade_report_message) if noti.has_grades_report_pdf_attachment
+    end
     remove_column :notifications, :has_grades_report_pdf_attachment, :boolean
   end
 
