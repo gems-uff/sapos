78c19a72bf356a884c3325aa761cf692f6131e78
SAASVUUV <lipeseabra@gmail.com>
2025-07-02 22:48:11 -0300

    ISSUE #566: Add the ordinance attribute to program_level and fix the acdemic transcript pdf to considerate the new information

---
+++ db/migrate/20250627152845_generate_ordinance_to_program_level.rb
@@ -0,0 +1,22 @@
+class GenerateOrdinanceToProgramLevel < ActiveRecord::Migration[7.0]
+  def change
+    Level.find_each do |level|
+      next if level.course_name.nil?
+      match = level.course_name.match(/\(([^)]+)\)/)
+      next unless match
+
+      ordinance_text = match[1]
+      year_match = ordinance_text.match(/(\d{4})$/)
+
+      year = year_match[1].to_i
+
+      ProgramLevel.find_each do |program|
+        if program.start_date.year < year && (program.end_date.nil? || program.end_date.year > year)
+          program.update!(ordinance: ordinance_text) if program.ordinance.nil?
+        end
+      end
+      new_course_name = level.course_name.sub(/\s*\([^)]+\)/, "")
+      level.update!(course_name: new_course_name)
+    end
+  end
+end
