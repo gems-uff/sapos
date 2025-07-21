c16b40d43fd096a1ad9f0f0576a5699fd356ca34
IMonardez <igormonardez@id.uff.br>
2025-07-21 09:37:39 -0300

    ISSUE #477 - Changes at update student migration

--- db/migrate/20250715181710_update_gender_student.rb
+++ db/migrate/20250715181710_update_gender_student.rb
@@ -1,8 +1,10 @@
 class UpdateGenderStudent < ActiveRecord::Migration[7.0]
   def change
-    actual_genders = I18n.t("active_scaffold.admissions/form_template.generate_fields.genders").keys
-    Student.where.not(gender: actual_genders) do |student|
+    actual_genders = I18n.t("active_scaffold.admissions/form_template.generate_fields.genders").values
+
+    Student.where.not(gender: actual_genders).each do |student|
       student.update(gender: nil)
+      student.save
     end
   end
 end
