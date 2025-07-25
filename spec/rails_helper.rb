ba734a36fbd9bfd79805b2b1716c4c1e4416da34
SAASVUUV <lipeseabra@gmail.com>
2025-07-24 21:34:50 -0300

    concat everything in a file lcvo.info, where the qlty program works

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,11 @@ require "spec_helper"
 require "simplecov"
 require "simplecov-lcov"
 
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = "coverage/lcov.info"
+end
+
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 
 SimpleCov.start "rails" do
