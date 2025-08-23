7a218bb7838d2bf5505b136b7aa139e2ad98d7d5
João Felipe Pimentel <joaofelipenp@gmail.com>
2025-08-23 18:58:55 -0300

    fix migration query

--- db/migrate/20250426173955_populate_user_roles.rb
+++ db/migrate/20250426173955_populate_user_roles.rb
@@ -3,8 +3,8 @@ class PopulateUserRoles < ActiveRecord::Migration[7.0]
     users_with_role = ActiveRecord::Base.connection.execute("SELECT id, role_id FROM users")
 
     users_with_role.each do |row|
-      user_id = row["id"]
-      role_id = row["role_id"]
+      user_id = row[0]
+      role_id = row[1]
 
       UserRole.create!(user_id: user_id, role_id: role_id) if role_id.present?
     end
