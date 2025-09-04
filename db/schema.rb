774e9234cf1201f790fabf934310b3df47fd2c8b
SAASVUUV <lipeseabra@gmail.com>
2025-09-04 18:23:14 -0300

    ISSUE #556: Run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_06_27_135451) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_03_205743) do
   create_table "accomplishments", force: :cascade do |t|
     t.integer "enrollment_id"
     t.integer "phase_id"
