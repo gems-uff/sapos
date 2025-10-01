f1179a7c01346b4c40adaf08d5fa3e63d14e6cff
IMonardez <igormonardez@id.uff.br>
2025-10-01 14:46:22 -0300

    ISSUE #477 - Fixing migration

--- db/migrate/20250715181710_update_gender_student.rb
+++ db/migrate/20250715181710_update_gender_student.rb
@@ -1,6 +1,6 @@
 class UpdateGenderStudent < ActiveRecord::Migration[7.0]
   def change
-    actual_genders = I18n.t("active_scaffold.admissions/form_template.generate_fields.genders").values - ['Não declarado']
+    actual_genders = I18n.t("active_scaffold.admissions/form_template.generate_fields.genders").values
 
     Student.where.not(gender: actual_genders).each do |student|
       student.update(gender: nil)
